9547233c9ed2f311f7e9c3aa1f10894196c6f0a6
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2026-07-25 21:05:22 +0200

    feat: add file_data_add_entry and file_data_get_length methods

--- src/file_data/file_data.c
+++ src/file_data/file_data.c
@@ -2,7 +2,9 @@
 #include "libft.h"
 
 
-struct s_file_data {};
+struct s_file_data {
+    t_file_data *next;
+};
 
 t_file_data *file_data_create()
 {
@@ -16,4 +18,30 @@ void file_data_destroy(t_file_data **_file_data)
 
     free(*_file_data);
     *_file_data = NULL;
+}
+
+void file_data_add_entry(t_file_data *_file_data, t_file_data *_next)
+{
+    if (_file_data == NULL)
+        return;
+
+    while (_file_data->next != NULL)
+    {
+        _file_data = _file_data->next;
+    }
+
+    _file_data->next = _next;
+}
+
+unsigned int file_data_get_length(const t_file_data *_file_data)
+{
+    unsigned int count = 0;
+
+    while (_file_data != NULL)
+    {
+        count++;
+        _file_data = _file_data->next;
+    }
+
+    return count;
 }
