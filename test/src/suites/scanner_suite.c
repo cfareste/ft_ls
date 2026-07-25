1f32e3a342da3ec3cd5b5b00a4e37daababd1755
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2026-07-25 20:22:23 +0200

    test: add should_return_one_entry_if_the_current_directory_has_one_file

--- test/src/suites/scanner_suite.c
+++ test/src/suites/scanner_suite.c
@@ -38,6 +38,19 @@ static void should_return_NULL_if_the_current_directory_is_empty(void)
     assertFileDataIsNull();
 }
 
+static void should_return_one_entry_if_the_current_directory_has_one_file(void)
+{
+    scan_directory(".");
+
+    int count = 0;
+    for (t_file_data *ptr = file_data; ptr != NULL; ptr = file_data_get_next(ptr))
+    {
+        count++;
+    }
+    CU_ASSERT_EQUAL(count, 0);
+    CU_ASSERT_STRING_EQUAL(file_data_get_name(file_data), "file");
+}
+
 void register_scanner_suite(void)
 {
     const CU_pSuite suite = CU_add_suite(SUITE_NAME, NULL, NULL);
@@ -47,5 +60,6 @@ void register_scanner_suite(void)
         CU_add_test(suite, "should_return_NULL_if_a_NULL_path_is_specified", should_return_NULL_if_a_NULL_path_is_specified);
         CU_add_test(suite, "should_return_NULL_if_an_empty_path_is_specified", should_return_NULL_if_an_empty_path_is_specified);
         CU_add_test(suite, "should_return_NULL_if_the_current_directory_is_empty", should_return_NULL_if_the_current_directory_is_empty);
+        CU_add_test(suite, "should_return_one_entry_if_the_current_directory_has_one_file", should_return_one_entry_if_the_current_directory_has_one_file);
     }
 }
