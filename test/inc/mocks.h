4106a27d3e0335781d5cf85de546f3ce6a5913eb
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2026-07-25 21:22:17 +0200

    test: add readdir mock

--- test/inc/mocks.h
+++ test/inc/mocks.h
@@ -1,6 +1,7 @@
 #pragma once
 
 typedef struct DIR DIR;
+struct dirent;
 
 typedef enum e_failure_type
 {
@@ -10,3 +11,4 @@ typedef enum e_failure_type
 extern t_failure_type g_opendir_fail;
 
 DIR *opendir_adapter(const char *path);
+struct dirent *readdir_adapter(DIR *dir);
--- test/src/mocks/dirent_mock.c
+++ test/src/mocks/dirent_mock.c
@@ -4,8 +4,10 @@
 #include "mocks.h"
 
 typedef struct DIR {} DIR;
+struct dirent {};
 
 t_failure_type g_opendir_fail = NO_FAILURE;
+t_failure_type g_readdir_fail = NO_FAILURE;
 
 DIR *opendir_adapter(const char *path)
 {
@@ -17,4 +19,16 @@ DIR *opendir_adapter(const char *path)
 
     DIR *dir = ft_calloc(1, sizeof(DIR));
     return dir;
+}
+
+struct dirent *readdir_adapter(DIR *dir)
+{
+    (void) dir;
+    if (g_readdir_fail != NO_FAILURE)
+    {
+        return NULL;
+    }
+
+    struct dirent *dirent = ft_calloc(1, sizeof(struct dirent));
+    return dirent;
 }
