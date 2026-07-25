043fba37f4816ff7688a4aec48f3e68ec48436d0
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2026-07-25 19:50:58 +0200

    test: add should_return_NULL_if_the_current_directory_is_empty

--- test/src/suites/scanner_suite.c
+++ test/src/suites/scanner_suite.c
@@ -19,6 +19,13 @@ static void should_return_NULL_if_an_empty_path_is_specified(void)
     CU_ASSERT_PTR_NULL(file_data);
 }
 
+static void should_return_NULL_if_the_current_directory_is_empty(void)
+{
+    const t_file_data *file_data = scan(".");
+
+    CU_ASSERT_PTR_NULL(file_data);
+}
+
 void register_scanner_suite(void)
 {
     const CU_pSuite suite = CU_add_suite(SUITE_NAME, NULL, NULL);
@@ -27,5 +34,6 @@ void register_scanner_suite(void)
     {
         CU_add_test(suite, "should_return_NULL_if_a_NULL_path_is_specified", should_return_NULL_if_a_NULL_path_is_specified);
         CU_add_test(suite, "should_return_NULL_if_an_empty_path_is_specified", should_return_NULL_if_an_empty_path_is_specified);
+        CU_add_test(suite, "should_return_NULL_if_the_current_directory_is_empty", should_return_NULL_if_the_current_directory_is_empty);
     }
 }
