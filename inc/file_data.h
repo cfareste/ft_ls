ca0096be129b73b801339fbc3e36d6913aca6207
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2026-07-25 17:39:28 +0200

    feat: add file_data_destroy method

--- inc/file_data.h
+++ inc/file_data.h
@@ -4,3 +4,4 @@ typedef struct s_file_data t_file_data;
 
 
 t_file_data *file_data_create();
+void file_data_destroy(t_file_data **_file_data);
--- src/file_data/file_data.c
+++ src/file_data/file_data.c
@@ -1,12 +1,20 @@
 #include "file_data.h"
 
-#include <stddef.h>
-
 #include "libft.h"
+
 
 struct s_file_data {};
 
 t_file_data *file_data_create()
 {
     return ft_calloc(1, sizeof(t_file_data));
+}
+
+void file_data_destroy(t_file_data **_file_data)
+{
+    if (!_file_data || !*_file_data)
+        return;
+
+    free(*_file_data);
+    *_file_data = NULL;
 }
