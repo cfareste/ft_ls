4ee4d6bce40e1b8bd693f5404beae66dbd1dce0e
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2026-07-26 11:12:22 +0200

    test: add affirm_readdir_will_return_N_files_named

--- test/inc/mocks.h
+++ test/inc/mocks.h
@@ -1,10 +1,14 @@
 #pragma once
 
-typedef struct DIR {} DIR;
 struct dirent
 {
     char d_name[256];
 };
+
+typedef struct DIR
+{
+    struct dirent entry;
+} DIR;
 
 typedef enum e_failure_type
 {
@@ -14,6 +18,5 @@ typedef enum e_failure_type
 extern t_failure_type g_opendir_fail;
 extern t_failure_type g_readdir_fail;
 
-DIR *opendir_adapter(const char *path);
-struct dirent *readdir_adapter(DIR *dir);
-void affirm_readdir_will_return_a_file_named(const char *path);
+void affirm_readdir_will_return_N_files_named(const char *file_name, unsigned int num);
+void affirm_readdir_will_return_a_file_named(const char *file_name);
--- test/src/mocks/dirent_mock.c
+++ test/src/mocks/dirent_mock.c
@@ -1,11 +1,27 @@
-#include <stdlib.h>
-#include <string.h>
-
+#include "libft.h"
+#include "ft_printf.h"
 #include "mocks.h"
 
 t_failure_type g_opendir_fail = NO_FAILURE;
 t_failure_type g_readdir_fail = NO_FAILURE;
 static char g_dirent_name[256] = "";
+static unsigned int g_dirent_entry_num = 0;
+static unsigned int g_total_dirent_entries = 0;
+
+static char *get_file_name()
+{
+    char *file_num = "";
+
+    if (g_dirent_entry_num != 1)
+        file_num = ft_uitoa(g_dirent_entry_num);
+
+    char *file_name = ft_strjoin(g_dirent_name, file_num);
+
+    if (g_dirent_entry_num != 1)
+        free(file_num);
+
+    return file_name;
+}
 
 DIR *opendir_adapter(const char *path)
 {
@@ -15,25 +31,44 @@ DIR *opendir_adapter(const char *path)
         return NULL;
     }
 
-    DIR *dir = calloc(1, sizeof(DIR));
+    DIR *dir = ft_calloc(1, sizeof(DIR));
+    g_dirent_name[0] = '\0';
+    g_dirent_entry_num = 0;
+    g_total_dirent_entries = 0;
+
     return dir;
 }
 
 struct dirent *readdir_adapter(DIR *dir)
 {
-    (void) dir;
     if (g_readdir_fail != NO_FAILURE)
     {
         return NULL;
     }
 
-    struct dirent *dirent = calloc(1, sizeof(struct dirent));
-    strlcpy(dirent->d_name, g_dirent_name, sizeof(dirent->d_name));
+    if (g_total_dirent_entries == 0 || g_dirent_entry_num > g_total_dirent_entries)
+    {
+        return NULL;
+    }
 
-    return dirent;
+
+    char *file_name = get_file_name();
+    ft_strlcpy(dir->entry.d_name, file_name, sizeof(dir->entry.d_name));
+    g_dirent_entry_num++;
+    free(file_name);
+
+    return &dir->entry;
 }
 
-void affirm_readdir_will_return_a_file_named(const char *path)
+//TODO: Change how the name file name is computed as it makes the tests fragile (we depend on fileX)
+void affirm_readdir_will_return_N_files_named(const char *file_name, const unsigned int num)
 {
-    strlcpy(g_dirent_name, path, sizeof(g_dirent_name));
+    g_dirent_entry_num = 1;
+    g_total_dirent_entries = num;
+    ft_strlcpy(g_dirent_name, file_name, sizeof(g_dirent_name));
+}
+
+void affirm_readdir_will_return_a_file_named(const char *file_name)
+{
+    affirm_readdir_will_return_N_files_named(file_name, 1);
 }
