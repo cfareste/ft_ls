5733afb64e579fb80e7b2b82697f946ffdafb204
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2026-07-27 22:36:53 +0200

    refactor: extract get_dir_stream method

--- src/scanner/scanner.c
+++ src/scanner/scanner.c
@@ -3,13 +3,24 @@
 #include "file_data.h"
 #include "os_adapter.h"
 
+static int is_invalid_path(const char *path)
+{
+    return path == NULL || path[0] == '\0';
+}
+
+static t_dir_stream *get_dir_stream(const char *path)
+{
+    if (is_invalid_path(path))
+        return NULL;
+
+    return opendir_adapter(path);
+}
+
 t_file_data *scan(const char *path)
 {
-    if (path == NULL) return NULL;
-    if (path[0] == '\0') return NULL;
-
-    t_dir_stream *dir_stream = opendir_adapter(path);
-    if (dir_stream == NULL) return NULL;
+    t_dir_stream *dir_stream = get_dir_stream(path);
+    if (dir_stream == NULL)
+        return NULL;
 
     t_file_data *file_data_list = NULL;
     t_dir_entry *dir_entry = readdir_adapter(dir_stream);
