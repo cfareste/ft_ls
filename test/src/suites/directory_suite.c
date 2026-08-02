a233d5f1a788708dc49740668bb0c3b9085572bc
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2026-08-02 11:49:36 +0200

    refactor(test): rename dir to dir_stream_sut

--- test/src/suites/directory_suite.c
+++ test/src/suites/directory_suite.c
@@ -4,37 +4,37 @@
 
 #define SUITE_NAME "directory"
 
-static DIR *dir;
+static DIR *dir_stream_sut;
 
 static void test_teardown(void)
 {
-    directory_close(dir);
+    directory_close(dir_stream_sut);
 }
 
 static void open_directory_stream(const char *path)
 {
-    dir = directory_open(path);
+    dir_stream_sut = directory_open(path);
 }
 
 static void should_return_NULL_when_opening_a_NULL_path(void)
 {
     open_directory_stream(NULL);
 
-    CU_ASSERT_PTR_NULL(dir);
+    CU_ASSERT_PTR_NULL(dir_stream_sut);
 }
 
 static void should_return_NULL_when_opening_an_empty_path(void)
 {
     open_directory_stream("");
 
-    CU_ASSERT_PTR_NULL(dir);
+    CU_ASSERT_PTR_NULL(dir_stream_sut);
 }
 
 static void should_return_a_directory_stream_when_opening_a_valid_path(void)
 {
     open_directory_stream("valid");
 
-    CU_ASSERT_PTR_NOT_NULL(dir);
+    CU_ASSERT_PTR_NOT_NULL(dir_stream_sut);
 }
 
 void register_directory_suite(void)
