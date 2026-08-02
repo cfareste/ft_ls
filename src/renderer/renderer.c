592a71f8cf124f74fed4713749525e0ee09f832c
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2026-08-02 22:43:55 +0200

    refactor: remove unnecessary iterator variables

--- src/renderer/renderer.c
+++ src/renderer/renderer.c
@@ -7,16 +7,13 @@ void render(const t_file_data *file_list)
     if (file_list == NULL)
         return;
 
-    unsigned int i = 0;
     const t_file_data *entry = file_list;
-    const unsigned int num_entries = file_data_get_length(file_list);
-    while (i < num_entries)
+    while (entry != NULL)
     {
         ft_printf("%s", file_data_get_name(entry));
-        if (i < num_entries - 1)
+        if (file_data_get_next(entry) != NULL)
             ft_printf("  ");
         entry = file_data_get_next(entry);
-        i++;
     }
     ft_printf("\n");
 }
