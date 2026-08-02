71b86978f753407e1a7512535d43ee396f1f4ff8
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2026-08-02 22:42:46 +0200

    feat: add should_print_the_name_of_every_entry_with_a_file_list_of_various_elements_separated_by_two_spaces

--- src/renderer/renderer.c
+++ src/renderer/renderer.c
@@ -7,5 +7,16 @@ void render(const t_file_data *file_list)
     if (file_list == NULL)
         return;
 
-    ft_printf("%s\n", file_data_get_name(file_list));
+    unsigned int i = 0;
+    const t_file_data *entry = file_list;
+    const unsigned int num_entries = file_data_get_length(file_list);
+    while (i < num_entries)
+    {
+        ft_printf("%s", file_data_get_name(entry));
+        if (i < num_entries - 1)
+            ft_printf("  ");
+        entry = file_data_get_next(entry);
+        i++;
+    }
+    ft_printf("\n");
 }
--- test/src/suites/renderer_suite.c
+++ test/src/suites/renderer_suite.c
@@ -29,6 +29,29 @@ static void should_print_the_name_of_the_entry_with_a_file_list_of_one_element(v
     file_data_destroy(&file_list);
 }
 
+static void should_print_the_name_of_every_entry_with_a_file_list_of_various_elements_separated_by_two_spaces(void)
+{
+    const char *expected_file_name[5] = { "file", "file2", "file3", "file4", "file5" };
+    t_file_data *file_list = file_data_create(expected_file_name[0]);
+    file_data_add_entry(&file_list, file_data_create(expected_file_name[1]));
+    file_data_add_entry(&file_list, file_data_create(expected_file_name[2]));
+    file_data_add_entry(&file_list, file_data_create(expected_file_name[3]));
+    file_data_add_entry(&file_list, file_data_create(expected_file_name[4]));
+
+    render(file_list);
+
+    verify_that_the_str_that_has_been_printed_is(
+        "%s  %s  %s  %s  %s\n",
+        expected_file_name[0],
+        expected_file_name[1],
+        expected_file_name[2],
+        expected_file_name[3],
+        expected_file_name[4]
+    );
+
+    file_data_destroy(&file_list);
+}
+
 void register_renderer_suite(void)
 {
     const CU_pSuite suite = CU_add_suite_with_setup_and_teardown(SUITE_NAME, NULL, NULL, test_setup, NULL);
@@ -37,5 +60,6 @@ void register_renderer_suite(void)
     {
         CU_add_test(suite, "should_not_print_anything_if_the_file_list_is_null", should_not_print_anything_if_the_file_list_is_null);
         CU_add_test(suite, "should_print_the_name_of_the_entry_with_a_file_list_of_one_element", should_print_the_name_of_the_entry_with_a_file_list_of_one_element);
+        CU_add_test(suite, "should_print_the_name_of_every_entry_with_a_file_list_of_various_elements_separated_by_two_spaces", should_print_the_name_of_every_entry_with_a_file_list_of_various_elements_separated_by_two_spaces);
     }
 }
