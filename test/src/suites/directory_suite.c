df5a007c9dcde5686bdb7f9dda93e389039a3b0d
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2026-08-02 11:39:28 +0200

    test: add should_return_a_directory_stream_when_opening_a_valid_path

--- test/src/suites/directory_suite.c
+++ test/src/suites/directory_suite.c
@@ -28,6 +28,15 @@ static void should_return_NULL_when_opening_an_empty_path(void)
     CU_ASSERT_PTR_NULL(dir);
 }
 
+static void should_return_a_directory_stream_when_opening_a_valid_path(void)
+{
+    DIR *dir = directory_open("valid");
+
+    CU_ASSERT_PTR_NOT_NULL(dir);
+
+    directory_close(dir);
+}
+
 void register_directory_suite(void)
 {
     const CU_pSuite suite = CU_add_suite_with_setup_and_teardown(SUITE_NAME, NULL, NULL, test_setup, test_teardown);
@@ -36,5 +45,6 @@ void register_directory_suite(void)
     {
         CU_add_test(suite, "should_return_NULL_when_opening_a_NULL_path", should_return_NULL_when_opening_a_NULL_path);
         CU_add_test(suite, "should_return_NULL_when_opening_an_empty_path", should_return_NULL_when_opening_an_empty_path);
+        CU_add_test(suite, "should_return_a_directory_stream_when_opening_a_valid_path", should_return_a_directory_stream_when_opening_a_valid_path);
     }
 }
