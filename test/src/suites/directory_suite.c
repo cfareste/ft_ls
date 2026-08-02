810daa953b7f2cfa8d65ee5a1c39d1fce9c218f4
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2026-08-02 11:21:47 +0200

    test: add should_return_NULL_when_opening_a_NULL_path

--- test/src/suites/directory_suite.c
+++ test/src/suites/directory_suite.c
@@ -1,5 +1,6 @@
 #include "CUnit/CUnit.h"
 #include "CUnit/Basic.h"
+#include "directory.h"
 
 #define SUITE_NAME "directory"
 
@@ -13,11 +14,19 @@ static void test_teardown(void)
 
 }
 
+static void should_return_NULL_when_opening_a_NULL_path(void)
+{
+    const DIR *dir = directory_open(NULL);
+
+    CU_ASSERT_PTR_NULL(dir);
+}
+
 void register_directory_suite(void)
 {
     const CU_pSuite suite = CU_add_suite_with_setup_and_teardown(SUITE_NAME, NULL, NULL, test_setup, test_teardown);
 
     if (suite != NULL)
     {
+        CU_add_test(suite, "should_return_NULL_when_opening_a_NULL_path", should_return_NULL_when_opening_a_NULL_path);
     }
 }
