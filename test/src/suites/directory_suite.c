df161fefcefccac1728a63adfe7168484f87434a
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2026-08-02 11:27:09 +0200

    test: add should_return_NULL_when_opening_an_empty_path

--- test/src/suites/directory_suite.c
+++ test/src/suites/directory_suite.c
@@ -21,6 +21,13 @@ static void should_return_NULL_when_opening_a_NULL_path(void)
     CU_ASSERT_PTR_NULL(dir);
 }
 
+static void should_return_NULL_when_opening_an_empty_path(void)
+{
+    const DIR *dir = directory_open("");
+
+    CU_ASSERT_PTR_NULL(dir);
+}
+
 void register_directory_suite(void)
 {
     const CU_pSuite suite = CU_add_suite_with_setup_and_teardown(SUITE_NAME, NULL, NULL, test_setup, test_teardown);
@@ -28,5 +35,6 @@ void register_directory_suite(void)
     if (suite != NULL)
     {
         CU_add_test(suite, "should_return_NULL_when_opening_a_NULL_path", should_return_NULL_when_opening_a_NULL_path);
+        CU_add_test(suite, "should_return_NULL_when_opening_an_empty_path", should_return_NULL_when_opening_an_empty_path);
     }
 }
