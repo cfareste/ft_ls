37b7377e405652d5057e68906ad9a946647553ab
cfareste <chrisfidare@yahoo.com>
2026-08-08 18:21:05 +0200

    refactor: improve printfs

--- src/renderer/renderer.c
+++ src/renderer/renderer.c
@@ -10,10 +10,7 @@ void render(const t_file_data *file_list)
     const t_file_data *entry = file_list;
     while (entry != NULL)
     {
-        ft_printf("%s", file_data_get_name(entry));
-        if (file_data_get_next(entry) != NULL)
-            ft_printf("\n");
+        ft_printf("%s\n", file_data_get_name(entry));
         entry = file_data_get_next(entry);
     }
-    ft_printf("\n");
 }
