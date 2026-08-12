8abe8b941442827721fe39366f40f455eb6948f3
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2026-08-12 18:29:16 +0200

    refactor: use ft_str_is_empty

--- src/filesystem/file_data.c
+++ src/filesystem/file_data.c
@@ -1,9 +1,9 @@
-#include <stdlib.h>
+#include "libft.h"
 #include "file_data.h"
 
 int get_file_data(const char *file_path, struct stat *stats)
 {
-    if (file_path == NULL || *file_path == '\0')
+    if (ft_is_str_empty(file_path))
         return FILE_DATA_COULD_NOT_RETRIEVE_DATA;
 
     return stat(file_path, stats);
