a16a7c200992c6ac9b22faa630dcc64955e5846c
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2026-08-14 21:29:23 +0200

    refactor(test): extract VALID_DIRECTORY_PATH macro

--- test/src/suites/directory_suite.c
+++ test/src/suites/directory_suite.c
@@ -4,6 +4,7 @@
 #include "mocks.h"
 
 #define SUITE_NAME "directory"
+#define VALID_DIRECTORY_PATH "valid"
 
 static DIR *dir_stream_sut;
 
@@ -43,17 +44,17 @@ static void should_return_NULL_when_opening_an_empty_path(void)
 
 static void should_return_a_directory_stream_when_opening_a_valid_path(void)
 {
-    ensure_opendir_will_open_a_dir_named("valid");
+    ensure_opendir_will_open_a_dir_named(VALID_DIRECTORY_PATH);
 
-    open_directory_stream("valid");
+    open_directory_stream(VALID_DIRECTORY_PATH);
 
     CU_ASSERT_PTR_NOT_NULL(dir_stream_sut);
 }
 
 static void should_return_NULL_when_reading_an_entry_from_a_null_directory(void)
 {
-    ensure_opendir_will_open_a_dir_named("valid");
-    open_directory_stream("valid");
+    ensure_opendir_will_open_a_dir_named(VALID_DIRECTORY_PATH);
+    open_directory_stream(VALID_DIRECTORY_PATH);
 
     const struct dirent *entry = directory_get_next_entry(NULL);
 
@@ -63,10 +64,10 @@ static void should_return_NULL_when_reading_an_entry_from_a_null_directory(void)
 static void should_return_an_entry_when_reading_from_a_valid_directory(void)
 {
     const char *file_name = "file";
-    ensure_opendir_will_open_a_dir_named("valid");
+    ensure_opendir_will_open_a_dir_named(VALID_DIRECTORY_PATH);
     guarantee_readdir_will_return_a_file_named(file_name);
 
-    open_directory_stream("valid");
+    open_directory_stream(VALID_DIRECTORY_PATH);
 
     const struct dirent *entry = directory_get_next_entry(dir_stream_sut);
 
@@ -83,8 +84,8 @@ static void should_return_minus_one_when_closing_a_null_directory(void)
 
 static void should_return_zero_when_closing_a_valid_directory(void)
 {
-    ensure_opendir_will_open_a_dir_named("valid");
-    open_directory_stream("valid");
+    ensure_opendir_will_open_a_dir_named(VALID_DIRECTORY_PATH);
+    open_directory_stream(VALID_DIRECTORY_PATH);
 
     const int actual = directory_close(&dir_stream_sut);
 
