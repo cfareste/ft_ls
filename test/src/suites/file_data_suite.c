2b5f29a044c244572c755ffb845ba1c34360233b
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2026-08-14 09:22:21 +0200

    test: add should_populate_successfully_a_valid_struct_when_passed_a_valid_directory_path

--- test/src/suites/file_data_suite.c
+++ test/src/suites/file_data_suite.c
@@ -46,6 +46,17 @@ static void should_populate_successfully_a_valid_struct_when_passed_a_valid_regu
     CU_ASSERT_EQUAL(valid_stat.st_mode, S_IFREG);
 }
 
+static void should_populate_successfully_a_valid_struct_when_passed_a_valid_directory_path(void)
+{
+    guarantee_stat_will_populate_data_of_a_directory_type_file();
+    struct stat valid_stat;
+
+    const int result = get_file_data("valid", &valid_stat);
+
+    CU_ASSERT_EQUAL(result, FILE_DATA_SUCCESS);
+    CU_ASSERT_EQUAL(valid_stat.st_mode, S_IFDIR);
+}
+
 void register_file_data_suite(void)
 {
     const CU_pSuite suite = CU_add_suite_with_setup_and_teardown(SUITE_NAME, NULL, NULL, test_setup, NULL);
@@ -56,5 +67,6 @@ void register_file_data_suite(void)
         CU_add_test(suite, "should_return_an_error_when_retrieving_the_data_from_an_empty_path", should_return_an_error_when_retrieving_the_data_from_an_empty_path);
         CU_add_test(suite, "should_return_an_error_when_populating_the_data_to_a_NULL_stat_struct", should_return_an_error_when_populating_the_data_to_a_NULL_stat_struct);
         CU_add_test(suite, "should_populate_successfully_a_valid_struct_when_passed_a_valid_regular_file_path", should_populate_successfully_a_valid_struct_when_passed_a_valid_regular_file_path);
+        CU_add_test(suite, "should_populate_successfully_a_valid_struct_when_passed_a_valid_directory_path", should_populate_successfully_a_valid_struct_when_passed_a_valid_directory_path);
     }
 }
