70b365044681d317242624a7b8bceff29141eb30
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2026-08-14 22:16:27 +0200

    test: add should_successfully_print_a_list_of_names_one_per_line_if_no_path_is_specified

--- test/src/suites/application_suite.c
+++ test/src/suites/application_suite.c
@@ -1,14 +1,44 @@
 #include "CUnit/CUnit.h"
 #include "CUnit/Basic.h"
+#include "mocks.h"
+#include "application.h"
 
 #define SUITE_NAME "application"
 
+static void test_setup(void)
+{
+    reset_dirent_guarantees();
+    reset_stat_guarantees();
+    reset_printing_buffer();
+}
+
+static void should_successfully_print_a_list_of_names_one_per_line_if_no_path_is_specified(void)
+{
+    const char *file_names[7] = { ".", "..", "file1", "subdir1", "symlink", "zz", NULL };
+    guarantee_stat_will_populate_stats_of_a_directory_type_file();
+    ensure_opendir_will_open_a_dir_named(".");
+    guarantee_readdir_will_return_N_files_named(file_names);
+
+    const int result = run_application();
+
+    verify_that_the_str_that_has_been_printed_is("%s\n%s\n%s\n%s\n%s\n%s\n",
+        file_names[0],
+        file_names[1],
+        file_names[2],
+        file_names[3],
+        file_names[4],
+        file_names[5],
+        file_names[6]
+    );
+    CU_ASSERT_EQUAL(result, FT_LS_APPLICATION_SUCCESS);
+}
+
 void register_application_suite(void)
 {
-    const CU_pSuite suite = CU_add_suite_with_setup_and_teardown(SUITE_NAME, NULL, NULL, NULL, NULL);
+    const CU_pSuite suite = CU_add_suite_with_setup_and_teardown(SUITE_NAME, NULL, NULL, test_setup, NULL);
 
     if (suite != NULL)
     {
-
+        CU_add_test(suite, "should_successfully_print_a_list_of_names_one_per_line_if_no_path_is_specified", should_successfully_print_a_list_of_names_one_per_line_if_no_path_is_specified);
     }
 }
