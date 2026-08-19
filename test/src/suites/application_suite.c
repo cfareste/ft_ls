d1bd9606349e47bfa9c377683a161caa557e962a
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2026-08-19 20:54:43 +0200

    refactor(test): extract parsed_arguments variable

--- test/src/suites/application_suite.c
+++ test/src/suites/application_suite.c
@@ -6,6 +6,8 @@
 
 #define SUITE_NAME "application"
 
+static t_parsed_arguments *parsed_arguments = NULL;
+
 static void test_setup(void)
 {
     reset_dirent_guarantees();
@@ -24,7 +26,7 @@ static void should_successfully_print_the_contents_of_the_current_directory_one_
     const char *dir_names[] = { ".", NULL };
     const char *file_names[7] = { ".", "..", "file1", "subdir1", "symlink", "zz", NULL };
     const char **entry_names[] = { file_names, NULL };
-    t_parsed_arguments *parsed_arguments = parse_arguments(0, arguments);
+    parsed_arguments = parse_arguments(0, arguments);
     guarantee_stat_will_populate_stats_of_a_directory_type_file(".");
     ensure_opendir_will_open_N_dirs_named(dir_names);
     guarantee_readdir_will_return_N_files_named(entry_names);
@@ -48,7 +50,7 @@ static void should_successfully_print_the_file_name_if_a_regular_file_operand_is
 {
     const char *file_name = "../../ft_ls/test/./frameworks/../regular_file";
     const char *arguments[] = { file_name, NULL };
-    t_parsed_arguments *parsed_arguments = parse_arguments(1, arguments);
+    parsed_arguments = parse_arguments(1, arguments);
     guarantee_stat_will_populate_stats_of_a_regular_type_file(file_name);
 
     const int result = run_application(parsed_arguments);
@@ -65,7 +67,7 @@ static void should_successfully_print_the_contents_of_the_directory_specified_as
     const char *dir_names[] = { arguments[0], NULL };
     const char *file_names[7] = { ".", "..", "file_from_dir_1", "subdir_1", "block_device", "char_device", NULL };
     const char **entry_names[] = { file_names, NULL };
-    t_parsed_arguments *parsed_arguments = parse_arguments(1, arguments);
+    parsed_arguments = parse_arguments(1, arguments);
     guarantee_stat_will_populate_stats_of_a_directory_type_file(arguments[0]);
     ensure_opendir_will_open_N_dirs_named(dir_names);
     guarantee_readdir_will_return_N_files_named(entry_names);
