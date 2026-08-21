7b700db2b7e434110d82b7127c7384cbd5b1ed28
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2026-08-21 22:14:39 +0200

    refactor(test): reorder mocking calls

--- test/src/suites/application_suite.c
+++ test/src/suites/application_suite.c
@@ -1,3 +1,4 @@
+#include <sys/stat.h>
 #include "CUnit/CUnit.h"
 #include "CUnit/Basic.h"
 #include "mocks.h"
@@ -31,10 +32,10 @@ static void should_successfully_print_the_contents_of_the_current_directory_one_
     const char *dir_names[] = { ".", NULL };
     const char *file_names[7] = { ".", "..", "file1", "subdir1", "symlink", "zz", NULL };
     const char **entry_names[] = { file_names, NULL };
-    parsed_arguments = parse_arguments(0, arguments);
     guarantee_stat_will_populate_stats_of_a_directory_type_file(".");
     ensure_opendir_will_open_N_dirs_named(dir_names);
     guarantee_readdir_will_return_N_files_named(entry_names);
+    parsed_arguments = parse_arguments(0, arguments);
 
     const int result = run_application(parsed_arguments);
 
@@ -53,8 +54,8 @@ static void should_successfully_print_the_file_name_if_a_regular_file_operand_is
 {
     const char *file_name = "../../ft_ls/test/./frameworks/../regular_file";
     const char *arguments[] = { file_name, NULL };
-    parsed_arguments = parse_arguments(1, arguments);
     guarantee_stat_will_populate_stats_of_a_regular_type_file(file_name);
+    parsed_arguments = parse_arguments(1, arguments);
 
     const int result = run_application(parsed_arguments);
 
@@ -68,10 +69,10 @@ static void should_successfully_print_the_contents_of_the_directory_specified_as
     const char *dir_names[] = { arguments[0], NULL };
     const char *file_names[7] = { ".", "..", "file_from_dir_1", "subdir_1", "block_device", "char_device", NULL };
     const char **entry_names[] = { file_names, NULL };
-    parsed_arguments = parse_arguments(1, arguments);
     guarantee_stat_will_populate_stats_of_a_directory_type_file(arguments[0]);
     ensure_opendir_will_open_N_dirs_named(dir_names);
     guarantee_readdir_will_return_N_files_named(entry_names);
+    parsed_arguments = parse_arguments(1, arguments);
 
     const int result = run_application(parsed_arguments);
 
@@ -92,8 +93,8 @@ void register_application_suite(void)
 
     if (suite != NULL)
     {
-        CU_add_test(suite, "should_successfully_print_the_contents_of_the_directory_specified_as_an_operand", should_successfully_print_the_contents_of_the_directory_specified_as_an_operand);
         CU_add_test(suite, "should_successfully_print_the_contents_of_the_current_directory_one_per_line_if_no_file_operands_are_specified", should_successfully_print_the_contents_of_the_current_directory_one_per_line_if_no_file_operands_are_specified);
         CU_add_test(suite, "should_successfully_print_the_file_name_if_a_regular_file_operand_is_specified", should_successfully_print_the_file_name_if_a_regular_file_operand_is_specified);
+        CU_add_test(suite, "should_successfully_print_the_contents_of_the_directory_specified_as_an_operand", should_successfully_print_the_contents_of_the_directory_specified_as_an_operand);
     }
 }
