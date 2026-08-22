77c1209eb15b4572aab68ad1abf07d71867565b6
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2026-08-22 20:39:51 +0200

    refactor: remove print_directory_header variable

--- src/renderer/renderer.c
+++ src/renderer/renderer.c
@@ -5,7 +5,6 @@
 struct s_render_config
 {
     char *directory_header;
-    int print_directory_header;
 };
 
 t_render_config *render_config_create_for_non_directory_file_operands()
@@ -13,7 +12,6 @@ t_render_config *render_config_create_for_non_directory_file_operands()
     t_render_config *config = ft_safe_calloc(1, sizeof(t_render_config));
 
     config->directory_header = NULL;
-    config->print_directory_header = 0;
 
     return config;
 }
@@ -23,7 +21,6 @@ t_render_config *render_config_create_for_directory_file_operands(const char *di
     t_render_config *config = ft_safe_calloc(1, sizeof(t_render_config));
 
     config->directory_header = ft_safe_strdup(directory_header);
-    config->print_directory_header = 1;
 
     return config;
 }
@@ -43,7 +40,7 @@ void render(const t_file_entry_list *file_entry_list, const t_render_config *con
     if (file_entry_list == NULL)
         return;
 
-    if (config->print_directory_header)
+    if (config->directory_header != NULL)
         ft_printf("\n%s:\n", config->directory_header);
 
     const t_file_entry_list *entry = file_entry_list;
