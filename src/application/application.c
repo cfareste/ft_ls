a7cdd4a5f21dee4828977f6dfd2409af49582a47
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2026-08-23 16:51:55 +0200

    refactor: pass render_context as argument

--- src/application/application.c
+++ src/application/application.c
@@ -5,23 +5,23 @@
 #include "libft.h"
 #include "scanner.h"
 
-static void process_non_directory_file_operands(const t_parsed_arguments *parsed_arguments)
+static void process_non_directory_file_operands(const t_parsed_arguments *parsed_arguments, t_render_context *render_context)
 {
     const char * const *non_directory_file_operands = parsed_arguments_get_non_directory_file_operands(parsed_arguments);
 
     for (unsigned int i = 0; non_directory_file_operands[i] != NULL; i++)
     {
         t_file_entry_list *file_entry_list = file_entry_list_create(non_directory_file_operands[i]);
-        t_render_context *non_directory_render_context = render_context_create_for_non_directory_file_operands();
+        render_context = render_context_create_for_non_directory_file_operands();
 
-        render(file_entry_list, non_directory_render_context);
+        render(file_entry_list, render_context);
 
         file_entry_list_destroy(&file_entry_list);
-        render_context_destroy(&non_directory_render_context);
+        render_context_destroy(&render_context);
     }
 }
 
-static void process_directory_file_operands(const t_parsed_arguments *parsed_arguments)
+static void process_directory_file_operands(const t_parsed_arguments *parsed_arguments, t_render_context *render_context)
 {
     const char * const *file_operands = parsed_arguments_get_file_operands(parsed_arguments);
     const char * const *non_directory_file_operands = parsed_arguments_get_non_directory_file_operands(parsed_arguments);
@@ -34,21 +34,23 @@ static void process_directory_file_operands(const t_parsed_arguments *parsed_arg
     for (unsigned int i = 0; directory_file_operands[i] != NULL; i++)
     {
         t_file_entry_list *file_entry_list = scan(directory_file_operands[i]);
-        t_render_context *directory_render_context = render_context_create_for_directory_file_operands(directory_file_operands[i]);
+        render_context = render_context_create_for_directory_file_operands(directory_file_operands[i]);
 
-        render_context_set_should_print_directory_header(directory_render_context, should_print_directory_header);
-        render_context_set_should_print_directory_header_leading_newline(directory_render_context, has_non_directory_file_operands || i > 0);
-        render(file_entry_list, directory_render_context);
+        render_context_set_should_print_directory_header(render_context, should_print_directory_header);
+        render_context_set_should_print_directory_header_leading_newline(render_context, has_non_directory_file_operands || i > 0);
+        render(file_entry_list, render_context);
 
         file_entry_list_destroy(&file_entry_list);
-        render_context_destroy(&directory_render_context);
+        render_context_destroy(&render_context);
     }
 }
 
 int run_application(const t_parsed_arguments *parsed_arguments)
 {
-    process_non_directory_file_operands(parsed_arguments);
-    process_directory_file_operands(parsed_arguments);
+    t_render_context *render_context = NULL;
+
+    process_non_directory_file_operands(parsed_arguments, render_context);
+    process_directory_file_operands(parsed_arguments, render_context);
 
     return FT_LS_APPLICATION_SUCCESS;
 }
