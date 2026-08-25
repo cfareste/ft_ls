0e98751c85ebf887fc69a68edfbcd2995a394d70
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2026-08-25 20:37:49 +0200

    refactor: extract is_hidden_file

--- src/scanner/scanner.c
+++ src/scanner/scanner.c
@@ -3,6 +3,11 @@
 #include "file_entry_list.h"
 #include "directory.h"
 
+static int is_hidden_file(const char *file_entry_name)
+{
+    return file_entry_name[0] == '.';
+}
+
 t_file_entry_list *scan(const char *path)
 {
     DIR *dir_stream = directory_open(path);
@@ -11,7 +16,7 @@ t_file_entry_list *scan(const char *path)
     const struct dirent *dir_entry = directory_get_next_entry(dir_stream);
     while (dir_entry != NULL)
     {
-        if (dir_entry->d_name[0] != '.')
+        if (!is_hidden_file(dir_entry->d_name))
         {
             t_file_entry_list *next = file_entry_list_create(dir_entry->d_name);
 
