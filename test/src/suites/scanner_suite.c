079b268f34a8ed98f8492d265f50e7b0f5fbdb22
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2026-08-26 17:30:12 +0200

    test: adapt should_return_NULL_if_the_current_directory_is_empty to new hidden file behavior

--- test/src/suites/scanner_suite.c
+++ test/src/suites/scanner_suite.c
@@ -70,15 +70,6 @@ static void should_return_NULL_if_an_empty_path_is_specified(void)
     assert_file_entry_list_is_null();
 }
 
-static void should_return_NULL_if_the_current_directory_is_empty(void)
-{
-    guarantee_stat_will_populate_stats_of_a_directory_type_file(CURRENT_DIRECTORY_PATH);
-
-    scan_directory(CURRENT_DIRECTORY_PATH);
-
-    assert_file_entry_list_is_null();
-}
-
 static void should_return_one_entry_if_the_current_directory_has_one_file(void)
 {
     const char *file_names[] = { "file", NULL };
@@ -154,6 +145,20 @@ static void should_return_a_list_of_entries_if_a_hidden_directory_is_specified(v
     assert_file_entry_list_names_are(expected_file_names);
 }
 
+static void should_return_NULL_if_the_current_directory_is_empty(void)
+{
+    const char *dir_names[] = { CURRENT_DIRECTORY_PATH, NULL };
+    const char *files_names[] = { ".", "..", NULL };
+    const char **entry_names[] = { files_names, NULL };
+    guarantee_stat_will_populate_stats_of_a_directory_type_file(CURRENT_DIRECTORY_PATH);
+    ensure_opendir_will_open_N_dirs_named(dir_names);
+    guarantee_readdir_will_return_N_files_named(entry_names);
+
+    scan_directory(CURRENT_DIRECTORY_PATH);
+
+    assert_file_entry_list_is_null();
+}
+
 static void should_return_NULL_if_fails_to_open_a_directory(void)
 {
     guarantee_stat_will_populate_stats_of_a_directory_type_file(CURRENT_DIRECTORY_PATH);
@@ -182,12 +187,12 @@ void register_scanner_suite(void)
     {
         CU_add_test(suite, "should_return_NULL_if_a_NULL_path_is_specified", should_return_NULL_if_a_NULL_path_is_specified);
         CU_add_test(suite, "should_return_NULL_if_an_empty_path_is_specified", should_return_NULL_if_an_empty_path_is_specified);
-        CU_add_test(suite, "should_return_NULL_if_the_current_directory_is_empty", should_return_NULL_if_the_current_directory_is_empty);
         CU_add_test(suite, "should_return_one_entry_if_the_current_directory_has_one_file", should_return_one_entry_if_the_current_directory_has_one_file);
         CU_add_test(suite, "should_return_multiple_entries_if_the_current_directory_has_more_than_one_file", should_return_multiple_entries_if_the_current_directory_has_more_than_one_file);
         CU_add_test(suite, "should_return_a_list_of_entries_if_one_non_empty_directory_path_is_specified", should_return_a_list_of_entries_if_one_non_empty_directory_path_is_specified);
         CU_add_test(suite, "should_return_a_list_of_entries_without_hidden_files_if_a_directory_with_hidden_files_is_specified", should_return_a_list_of_entries_without_hidden_files_if_a_directory_with_hidden_files_is_specified);
         CU_add_test(suite, "should_return_a_list_of_entries_if_a_hidden_directory_is_specified", should_return_a_list_of_entries_if_a_hidden_directory_is_specified);
+        CU_add_test(suite, "should_return_NULL_if_the_current_directory_is_empty", should_return_NULL_if_the_current_directory_is_empty);
         CU_add_test(suite, "should_return_NULL_if_fails_to_open_a_directory", should_return_NULL_if_fails_to_open_a_directory);
         CU_add_test(suite, "should_return_NULL_if_fails_to_read_a_directory", should_return_NULL_if_fails_to_read_a_directory);
     }
