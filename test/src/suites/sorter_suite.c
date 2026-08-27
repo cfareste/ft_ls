2f5b20d6828cad3341932d9cb93a11cdb3fafe41
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2026-08-27 16:38:18 +0200

    test: add should_not_sort_when_an_already_sorted_list_is_passed

--- test/src/suites/sorter_suite.c
+++ test/src/suites/sorter_suite.c
@@ -28,6 +28,27 @@ static void should_not_sort_when_a_list_with_one_item_is_passed(void)
     file_entry_list_destroy(&file_entry_list);
 }
 
+static void should_not_sort_when_an_already_sorted_list_is_passed(void)
+{
+    t_file_entry_list *file_entry_list = file_entry_list_create("file");
+    file_entry_list_add_entry(&file_entry_list, file_entry_list_create("file2"));
+    file_entry_list_add_entry(&file_entry_list, file_entry_list_create("file3"));
+    file_entry_list_add_entry(&file_entry_list, file_entry_list_create("file4"));
+
+    sort(&file_entry_list);
+    const t_file_entry_list *second_entry = file_entry_list_get_next(file_entry_list);
+    const t_file_entry_list *third_entry = file_entry_list_get_next(second_entry);
+    const t_file_entry_list *fourth_entry = file_entry_list_get_next(third_entry);
+
+    CU_ASSERT_STRING_EQUAL(file_entry_list_get_name(file_entry_list), "file");
+    CU_ASSERT_STRING_EQUAL(file_entry_list_get_name(second_entry), "file2");
+    CU_ASSERT_STRING_EQUAL(file_entry_list_get_name(third_entry), "file3");
+    CU_ASSERT_STRING_EQUAL(file_entry_list_get_name(fourth_entry), "file4");
+    CU_ASSERT_EQUAL(file_entry_list_get_length(file_entry_list), 4);
+
+    file_entry_list_destroy(&file_entry_list);
+}
+
 void register_sorter_suite(void)
 {
     const CU_pSuite suite = CU_add_suite_with_setup_and_teardown(SUITE_NAME, NULL, NULL, NULL, NULL);
@@ -37,5 +58,6 @@ void register_sorter_suite(void)
         CU_add_test(suite, "should_not_fail_when_passed_a_NULL_pointer", should_not_fail_when_passed_a_NULL_pointer);
         CU_add_test(suite, "should_not_fail_when_passed_a_pointer_pointing_at_NULL", should_not_fail_when_passed_a_pointer_pointing_at_NULL);
         CU_add_test(suite, "should_not_sort_when_a_list_with_one_item_is_passed", should_not_sort_when_a_list_with_one_item_is_passed);
+        CU_add_test(suite, "should_not_sort_when_an_already_sorted_list_is_passed", should_not_sort_when_an_already_sorted_list_is_passed);
     }
 }
