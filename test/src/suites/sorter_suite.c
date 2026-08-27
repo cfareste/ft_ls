d2724316fd9006cb4b370520676c2b53cdee3851
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2026-08-27 16:20:00 +0200

    test: add should_not_sort_when_a_list_with_one_item_is_passed

--- test/src/suites/sorter_suite.c
+++ test/src/suites/sorter_suite.c
@@ -16,6 +16,18 @@ static void should_not_fail_when_passed_a_pointer_pointing_at_NULL(void)
     sort(&invalid_list);
 }
 
+static void should_not_sort_when_a_list_with_one_item_is_passed(void)
+{
+    t_file_entry_list *file_entry_list = file_entry_list_create("file");
+
+    sort(&file_entry_list);
+
+    CU_ASSERT_STRING_EQUAL(file_entry_list_get_name(file_entry_list), "file");
+    CU_ASSERT_EQUAL(file_entry_list_get_length(file_entry_list), 1);
+
+    file_entry_list_destroy(&file_entry_list);
+}
+
 void register_sorter_suite(void)
 {
     const CU_pSuite suite = CU_add_suite_with_setup_and_teardown(SUITE_NAME, NULL, NULL, NULL, NULL);
@@ -24,5 +36,6 @@ void register_sorter_suite(void)
     {
         CU_add_test(suite, "should_not_fail_when_passed_a_NULL_pointer", should_not_fail_when_passed_a_NULL_pointer);
         CU_add_test(suite, "should_not_fail_when_passed_a_pointer_pointing_at_NULL", should_not_fail_when_passed_a_pointer_pointing_at_NULL);
+        CU_add_test(suite, "should_not_sort_when_a_list_with_one_item_is_passed", should_not_sort_when_a_list_with_one_item_is_passed);
     }
 }
