429878328f52d7612efe4b5d3c40a46974e7cc3e
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2026-08-27 16:15:15 +0200

    test: add should_not_fail_when_passed_a_pointer_pointing_at_NULL

--- test/src/suites/sorter_suite.c
+++ test/src/suites/sorter_suite.c
@@ -9,6 +9,13 @@ static void should_not_fail_when_passed_a_NULL_pointer(void)
     sort(NULL);
 }
 
+static void should_not_fail_when_passed_a_pointer_pointing_at_NULL(void)
+{
+    t_file_entry_list *invalid_list = NULL;
+
+    sort(&invalid_list);
+}
+
 void register_sorter_suite(void)
 {
     const CU_pSuite suite = CU_add_suite_with_setup_and_teardown(SUITE_NAME, NULL, NULL, NULL, NULL);
@@ -16,5 +23,6 @@ void register_sorter_suite(void)
     if (suite != NULL)
     {
         CU_add_test(suite, "should_not_fail_when_passed_a_NULL_pointer", should_not_fail_when_passed_a_NULL_pointer);
+        CU_add_test(suite, "should_not_fail_when_passed_a_pointer_pointing_at_NULL", should_not_fail_when_passed_a_pointer_pointing_at_NULL);
     }
 }
