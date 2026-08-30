3365d2cece4655d2fdb333589fca15cafb3ce335
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2026-08-30 19:54:05 +0200

    refactor(test): reorder tests

--- test/src/suites/file_entry_suite.c
+++ test/src/suites/file_entry_suite.c
@@ -174,20 +174,6 @@ static void should_return_NULL_for_an_entry_if_an_out_of_bounds_index_is_specifi
     CU_ASSERT_PTR_NULL(file_entry_array_get_at(sut, -1));
 }
 
-static void should_not_fail_pushing_an_entry_to_a_NULL_array(void)
-{
-    t_file_entry *entry = file_entry_create("valid");
-
-    file_entry_array_push(NULL, entry);
-
-    file_entry_destroy(&entry);
-}
-
-static void should_not_fail_pushing_a_NULL_entry_to_an_array(void)
-{
-    file_entry_array_push(sut, NULL);
-}
-
 static void should_return_the_entry_at_the_specified_index(void)
 {
     file_entry_array_push(sut, file_entry_create("entry 0"));
@@ -208,6 +194,20 @@ static void should_return_the_entry_at_the_specified_index(void)
     CU_ASSERT_STRING_EQUAL(second_entry_name, "entry 1");
     CU_ASSERT_STRING_EQUAL(third_entry_name, "entry 2");
     CU_ASSERT_STRING_EQUAL(fourth_entry_name, "entry 3");
+}
+
+static void should_not_fail_pushing_an_entry_to_a_NULL_array(void)
+{
+    t_file_entry *entry = file_entry_create("valid");
+
+    file_entry_array_push(NULL, entry);
+
+    file_entry_destroy(&entry);
+}
+
+static void should_not_fail_pushing_a_NULL_entry_to_an_array(void)
+{
+    file_entry_array_push(sut, NULL);
 }
 
 void register_file_entry_suite(void)
@@ -235,8 +235,8 @@ void register_file_entry_suite(void)
         CU_add_test(suite, "should_return_the_correct_length", should_return_the_correct_length);
         CU_add_test(suite, "should_return_NULL_for_an_entry_if_a_NULL_array_is_passed", should_return_NULL_for_an_entry_if_a_NULL_array_is_passed);
         CU_add_test(suite, "should_return_NULL_for_an_entry_if_an_out_of_bounds_index_is_specified", should_return_NULL_for_an_entry_if_an_out_of_bounds_index_is_specified);
+        CU_add_test(suite, "should_return_the_entry_at_the_specified_index", should_return_the_entry_at_the_specified_index);
         CU_add_test(suite, "should_not_fail_pushing_an_entry_to_a_NULL_array", should_not_fail_pushing_an_entry_to_a_NULL_array);
         CU_add_test(suite, "should_not_fail_pushing_a_NULL_entry_to_an_array", should_not_fail_pushing_a_NULL_entry_to_an_array);
-        CU_add_test(suite, "should_return_the_entry_at_the_specified_index", should_return_the_entry_at_the_specified_index);
     }
 }
