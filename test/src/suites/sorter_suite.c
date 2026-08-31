f1defb6ae28ef8342d5792f7b3f8c46a3dc5dd6e
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2026-08-31 14:59:50 +0200

    test: change sorter tests to use void *

--- test/src/suites/sorter_suite.c
+++ test/src/suites/sorter_suite.c
@@ -1,7 +1,6 @@
 #include "CUnit/CUnit.h"
 #include "CUnit/Basic.h"
 #include "sorter.h"
-#include "file_entry.h"
 #include "libft.h"
 
 #define SUITE_NAME "sorter"
@@ -25,39 +24,27 @@ static void should_not_fail_when_passing_a_NULL_file_entry_array(void)
 
 static void should_do_nothing_when_a_file_entry_array_with_one_entry_is_passed(void)
 {
-    t_file_entry_array *file_entry_array = file_entry_array_create();
-    t_file_entry *file_entry = file_entry_create("file");
-    file_entry_array_push(file_entry_array, file_entry);
+    void *array[] = { "file", NULL };
 
-    sort_pointer_array((void **) &file_entry_array, compare_by_ascii);
+    sort_pointer_array(array, compare_by_ascii);
 
-    CU_ASSERT_STRING_EQUAL(file_entry_get_name(file_entry_array_get_at(file_entry_array, 0)), "file");
-    CU_ASSERT_EQUAL(file_entry_array_get_length(file_entry_array), 1);
-
-    file_entry_array_destroy(&file_entry_array);
+    CU_ASSERT_STRING_EQUAL(array[0], "file");
+    CU_ASSERT_PTR_NULL(array[1]);
 }
 
 static void should_do_nothing_when_an_already_sorted_file_entry_array_is_passed(void)
 {
-    t_file_entry_array *file_entry_array = file_entry_array_create();
-    file_entry_array_push(file_entry_array, file_entry_create("file"));
-    file_entry_array_push(file_entry_array, file_entry_create("file2"));
-    file_entry_array_push(file_entry_array, file_entry_create("file3"));
-    file_entry_array_push(file_entry_array, file_entry_create("file4"));
+    void *array[] = { "file", "_file", "f", "a", "2file", "FILE", NULL };
 
-    sort_pointer_array((void **) &file_entry_array, compare_by_ascii);
-    const t_file_entry *first_entry = file_entry_array_get_at(file_entry_array, 0);
-    const t_file_entry *second_entry = file_entry_array_get_at(file_entry_array, 1);
-    const t_file_entry *third_entry = file_entry_array_get_at(file_entry_array, 2);
-    const t_file_entry *fourth_entry = file_entry_array_get_at(file_entry_array, 3);
+    sort_pointer_array(array, compare_by_ascii);
 
-    CU_ASSERT_STRING_EQUAL(file_entry_get_name(first_entry), "file");
-    CU_ASSERT_STRING_EQUAL(file_entry_get_name(second_entry), "file2");
-    CU_ASSERT_STRING_EQUAL(file_entry_get_name(third_entry), "file3");
-    CU_ASSERT_STRING_EQUAL(file_entry_get_name(fourth_entry), "file4");
-    CU_ASSERT_EQUAL(file_entry_array_get_length(file_entry_array), 4);
-
-    file_entry_array_destroy(&file_entry_array);
+    CU_ASSERT_STRING_EQUAL(array[0], "file");
+    CU_ASSERT_STRING_EQUAL(array[1], "_file");
+    CU_ASSERT_STRING_EQUAL(array[2], "f");
+    CU_ASSERT_STRING_EQUAL(array[3], "a");
+    CU_ASSERT_STRING_EQUAL(array[4], "2file");
+    CU_ASSERT_STRING_EQUAL(array[5], "FILE");
+    CU_ASSERT_PTR_NULL(array[6]);
 }
 
 void register_sorter_suite(void)
