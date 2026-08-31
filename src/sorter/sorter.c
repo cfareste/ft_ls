15f364e47c4b1674ed9f7248c96bc6162fb74b9a
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2026-08-31 17:35:56 +0200

    feat: add sorting method

--- src/sorter/sorter.c
+++ src/sorter/sorter.c
@@ -1,7 +1,21 @@
+#include <stddef.h>
 #include "sorter.h"
 
 void sort_pointer_array(void **array, const t_comparator_fn compare_fn)
 {
-    (void) *array;
-    (void) compare_fn;
+    if (array == NULL || *array == NULL)
+        return;
+
+    for (unsigned int i = 0; array[i] != NULL; i++)
+    {
+        for (unsigned int j = i + 1; array[j] != NULL; j++)
+        {
+            if (compare_fn(array[i], array[j]) < 0)
+                continue ;
+
+            void *tmp = array[i];
+            array[i] = array[j];
+            array[j] = tmp;
+        }
+    }
 }
--- test/src/suites/sorter_suite.c
+++ test/src/suites/sorter_suite.c
@@ -48,6 +48,22 @@ static void should_do_nothing_when_an_already_sorted_file_entry_array_is_passed(
     CU_ASSERT_PTR_NULL(array[7]);
 }
 
+static void should_sort_an_unsorted_array(void)
+{
+    void *array[] = { "a", "2file", ".hidden", "_file", "f", "file", "FILE", NULL };
+
+    sort_pointer_array(array, compare_by_ascii);
+
+    CU_ASSERT_STRING_EQUAL(array[0], ".hidden");
+    CU_ASSERT_STRING_EQUAL(array[1], "2file");
+    CU_ASSERT_STRING_EQUAL(array[2], "FILE");
+    CU_ASSERT_STRING_EQUAL(array[3], "_file");
+    CU_ASSERT_STRING_EQUAL(array[4], "a");
+    CU_ASSERT_STRING_EQUAL(array[5], "f");
+    CU_ASSERT_STRING_EQUAL(array[6], "file");
+    CU_ASSERT_PTR_NULL(array[7]);
+}
+
 void register_sorter_suite(void)
 {
     const CU_pSuite suite = CU_add_suite_with_setup_and_teardown(SUITE_NAME, NULL, NULL, NULL, NULL);
@@ -58,5 +74,6 @@ void register_sorter_suite(void)
         CU_add_test(suite, "should_not_fail_when_passing_a_NULL_file_entry_array", should_not_fail_when_passing_a_NULL_file_entry_array);
         CU_add_test(suite, "should_do_nothing_when_a_file_entry_array_with_one_entry_is_passed", should_do_nothing_when_a_file_entry_array_with_one_entry_is_passed);
         CU_add_test(suite, "should_do_nothing_when_an_already_sorted_file_entry_array_is_passed", should_do_nothing_when_an_already_sorted_file_entry_array_is_passed);
+        CU_add_test(suite, "should_sort_an_unsorted_array", should_sort_an_unsorted_array);
     }
 }
