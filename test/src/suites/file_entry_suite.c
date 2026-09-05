dc5a8c54e750a8ef1fe8d66f3776e807e19159fe
cfareste <chrisfidare@yahoo.com>
2026-09-05 20:04:52 +0200

    refactor(test): extract entry_sut and rename array_sut

--- test/src/suites/file_entry_suite.c
+++ test/src/suites/file_entry_suite.c
@@ -5,16 +5,19 @@
 #define SUITE_NAME "file_entry"
 #define DEFAULT_NAME "default"
 
-static t_file_entry_array *sut;
+static t_file_entry *entry_sut;
+static t_file_entry_array *array_sut;
 
 static void test_setup(void)
 {
-    sut = file_entry_array_create();
+    entry_sut = file_entry_create(DEFAULT_NAME);
+    array_sut = file_entry_array_create();
 }
 
 static void test_teardown(void)
 {
-    file_entry_array_destroy(&sut);
+    file_entry_destroy(&entry_sut);
+    file_entry_array_destroy(&array_sut);
 }
 
 static void assert_file_entry_array_is_null(const t_file_entry_array *file_entry_array)
@@ -24,17 +27,13 @@ static void assert_file_entry_array_is_null(const t_file_entry_array *file_entry
 
 static void should_create_file_entry_correctly(void)
 {
-    t_file_entry *file_entry = file_entry_create(DEFAULT_NAME);
-
-    CU_ASSERT_PTR_NOT_NULL(file_entry);
-    CU_ASSERT_STRING_EQUAL(file_entry_get_name(file_entry), DEFAULT_NAME);
-
-    file_entry_destroy(&file_entry);
+    CU_ASSERT_PTR_NOT_NULL(entry_sut);
+    CU_ASSERT_STRING_EQUAL(file_entry_get_name(entry_sut), DEFAULT_NAME);
 }
 
 static void should_create_file_entry_array_correctly(void)
 {
-    CU_ASSERT_PTR_NOT_NULL(sut);
+    CU_ASSERT_PTR_NOT_NULL(array_sut);
 }
 
 static void should_fail_to_create_an_entry_if_a_NULL_file_name_is_passed(void)
@@ -53,21 +52,19 @@ static void should_fail_to_create_an_entry_if_an_empty_file_name_is_passed(void)
 
 static void should_destroy_file_entry_correctly(void)
 {
-    t_file_entry *entry = file_entry_create("valid");
+    file_entry_destroy(&entry_sut);
 
-    file_entry_destroy(&entry);
-
-    CU_ASSERT_PTR_NULL(entry);
+    CU_ASSERT_PTR_NULL(entry_sut);
 }
 
 static void should_destroy_file_entry_array_correctly(void)
 {
-    file_entry_array_push(sut, file_entry_create("entry"));
-    file_entry_array_push(sut, file_entry_create("entry2"));
+    file_entry_array_push(array_sut, file_entry_create("entry"));
+    file_entry_array_push(array_sut, file_entry_create("entry2"));
 
-    file_entry_array_destroy(&sut);
+    file_entry_array_destroy(&array_sut);
 
-    assert_file_entry_array_is_null(sut);
+    assert_file_entry_array_is_null(array_sut);
 }
 
 static void should_not_fail_to_destroy_a_file_entry_when_a_null_pointer_is_passed(void)
@@ -89,8 +86,8 @@ static void should_not_fail_to_destroy_a_file_entry_array_when_a_null_pointer_is
 
 static void should_not_fail_to_destroy_a_file_entry_array_that_is_already_null(void)
 {
-    file_entry_array_destroy(&sut);
-    file_entry_array_destroy(&sut);
+    file_entry_array_destroy(&array_sut);
+    file_entry_array_destroy(&array_sut);
 }
 
 static void should_return_NULL_for_the_entry_name_if_a_NULL_entry_is_passed(void)
@@ -107,38 +104,29 @@ static void should_not_fail_setting_the_entry_name_if_a_NULL_file_entry_is_passe
 
 static void should_not_set_the_entry_name_if_a_NULL_file_name_is_passed(void)
 {
-    t_file_entry *entry = file_entry_create("valid");
-    file_entry_set_name(entry, NULL);
+    file_entry_set_name(entry_sut, NULL);
 
-    const char *entry_name = file_entry_get_name(entry);
+    const char *entry_name = file_entry_get_name(entry_sut);
 
-    CU_ASSERT_STRING_EQUAL(entry_name, "valid");
-
-    file_entry_destroy(&entry);
+    CU_ASSERT_STRING_EQUAL(entry_name, DEFAULT_NAME);
 }
 
 static void should_not_set_the_entry_name_if_an_empty_file_name_is_passed(void)
 {
-    t_file_entry *entry = file_entry_create("valid");
-    file_entry_set_name(entry, "");
+    file_entry_set_name(entry_sut, "");
 
-    const char *entry_name = file_entry_get_name(entry);
+    const char *entry_name = file_entry_get_name(entry_sut);
 
-    CU_ASSERT_STRING_EQUAL(entry_name, "valid");
-
-    file_entry_destroy(&entry);
+    CU_ASSERT_STRING_EQUAL(entry_name, DEFAULT_NAME);
 }
 
 static void should_return_the_entry_name_correctly(void)
 {
-    t_file_entry *file_entry = file_entry_create("name");
-    file_entry_set_name(file_entry, "changed name");
+    file_entry_set_name(entry_sut, "changed name");
 
-    const char *entry_name = file_entry_get_name(file_entry);
+    const char *entry_name = file_entry_get_name(entry_sut);
 
     CU_ASSERT_STRING_EQUAL(entry_name, "changed name");
-
-    file_entry_destroy(&file_entry);
 }
 
 static void should_return_a_length_of_zero_when_passed_a_NULL_file_entry_array(void)
@@ -150,10 +138,10 @@ static void should_return_a_length_of_zero_when_passed_a_NULL_file_entry_array(v
 
 static void should_return_the_correct_length(void)
 {
-    file_entry_array_push(sut, file_entry_create(DEFAULT_NAME));
-    file_entry_array_push(sut, file_entry_create(DEFAULT_NAME));
+    file_entry_array_push(array_sut, file_entry_create(DEFAULT_NAME));
+    file_entry_array_push(array_sut, file_entry_create(DEFAULT_NAME));
 
-    CU_ASSERT_EQUAL(file_entry_array_get_length(sut), 2);
+    CU_ASSERT_EQUAL(file_entry_array_get_length(array_sut), 2);
 }
 
 static void should_return_NULL_for_an_entry_if_a_NULL_array_is_passed(void)
@@ -165,26 +153,26 @@ static void should_return_NULL_for_an_entry_if_a_NULL_array_is_passed(void)
 
 static void should_return_NULL_for_an_entry_if_an_out_of_bounds_index_is_specified(void)
 {
-    file_entry_array_push(sut, file_entry_create("entry 0"));
-    file_entry_array_push(sut, file_entry_create("entry 1"));
-    file_entry_array_push(sut, file_entry_create("entry 2"));
+    file_entry_array_push(array_sut, file_entry_create("entry 0"));
+    file_entry_array_push(array_sut, file_entry_create("entry 1"));
+    file_entry_array_push(array_sut, file_entry_create("entry 2"));
 
-    CU_ASSERT_PTR_NULL(file_entry_array_get_at(sut, 3));
-    CU_ASSERT_PTR_NULL(file_entry_array_get_at(sut, 17));
-    CU_ASSERT_PTR_NULL(file_entry_array_get_at(sut, -1));
+    CU_ASSERT_PTR_NULL(file_entry_array_get_at(array_sut, 3));
+    CU_ASSERT_PTR_NULL(file_entry_array_get_at(array_sut, 17));
+    CU_ASSERT_PTR_NULL(file_entry_array_get_at(array_sut, -1));
 }
 
 static void should_return_the_entry_at_the_specified_index(void)
 {
-    file_entry_array_push(sut, file_entry_create("entry 0"));
-    file_entry_array_push(sut, file_entry_create("entry 1"));
-    file_entry_array_push(sut, file_entry_create("entry 2"));
-    file_entry_array_push(sut, file_entry_create("entry 3"));
+    file_entry_array_push(array_sut, file_entry_create("entry 0"));
+    file_entry_array_push(array_sut, file_entry_create("entry 1"));
+    file_entry_array_push(array_sut, file_entry_create("entry 2"));
+    file_entry_array_push(array_sut, file_entry_create("entry 3"));
 
-    const t_file_entry *first_entry = file_entry_array_get_at(sut, 0);
-    const t_file_entry *second_entry = file_entry_array_get_at(sut, 1);
-    const t_file_entry *third_entry = file_entry_array_get_at(sut, 2);
-    const t_file_entry *fourth_entry = file_entry_array_get_at(sut, 3);
+    const t_file_entry *first_entry = file_entry_array_get_at(array_sut, 0);
+    const t_file_entry *second_entry = file_entry_array_get_at(array_sut, 1);
+    const t_file_entry *third_entry = file_entry_array_get_at(array_sut, 2);
+    const t_file_entry *fourth_entry = file_entry_array_get_at(array_sut, 3);
     const char *first_entry_name = file_entry_get_name(first_entry);
     const char *second_entry_name = file_entry_get_name(second_entry);
     const char *third_entry_name = file_entry_get_name(third_entry);
@@ -198,31 +186,27 @@ static void should_return_the_entry_at_the_specified_index(void)
 
 static void should_not_fail_pushing_an_entry_to_a_NULL_array(void)
 {
-    t_file_entry *entry = file_entry_create("valid");
-
-    file_entry_array_push(NULL, entry);
-
-    file_entry_destroy(&entry);
+    file_entry_array_push(NULL, entry_sut);
 }
 
 static void should_not_fail_pushing_a_NULL_entry_to_an_array(void)
 {
-    file_entry_array_push(sut, NULL);
+    file_entry_array_push(array_sut, NULL);
 }
 
 static void should_push_correctly_an_specified_entry(void)
 {
     for (unsigned int i = 0; i < 10; i++)
     {
-        file_entry_array_push(sut, file_entry_create("entry"));
+        file_entry_array_push(array_sut, file_entry_create("entry"));
     }
 
     unsigned int i = 0;
     for (; i < 10; i++)
     {
-        CU_ASSERT_PTR_NOT_NULL(file_entry_array_get_at(sut, i));
+        CU_ASSERT_PTR_NOT_NULL(file_entry_array_get_at(array_sut, i));
     }
-    CU_ASSERT_PTR_NULL(file_entry_array_get_at(sut, i));
+    CU_ASSERT_PTR_NULL(file_entry_array_get_at(array_sut, i));
 }
 
 static void should_not_fail_when_sorting_a_NULL_file_entry_array(void)
@@ -233,24 +217,24 @@ static void should_not_fail_when_sorting_a_NULL_file_entry_array(void)
 static void should_sort_the_file_operands_by_entry_name(void)
 {
     const char *entry_names[] = { "2file", ".hiddir", "_file", "dir", "FILE", "zfile", "zzfile", "zz", NULL };
-    file_entry_array_push(sut, file_entry_create(entry_names[0]));
-    file_entry_array_push(sut, file_entry_create(entry_names[1]));
-    file_entry_array_push(sut, file_entry_create(entry_names[2]));
-    file_entry_array_push(sut, file_entry_create(entry_names[3]));
-    file_entry_array_push(sut, file_entry_create(entry_names[4]));
-    file_entry_array_push(sut, file_entry_create(entry_names[5]));
-    file_entry_array_push(sut, file_entry_create(entry_names[6]));
-    file_entry_array_push(sut, file_entry_create(entry_names[7]));
+    file_entry_array_push(array_sut, file_entry_create(entry_names[0]));
+    file_entry_array_push(array_sut, file_entry_create(entry_names[1]));
+    file_entry_array_push(array_sut, file_entry_create(entry_names[2]));
+    file_entry_array_push(array_sut, file_entry_create(entry_names[3]));
+    file_entry_array_push(array_sut, file_entry_create(entry_names[4]));
+    file_entry_array_push(array_sut, file_entry_create(entry_names[5]));
+    file_entry_array_push(array_sut, file_entry_create(entry_names[6]));
+    file_entry_array_push(array_sut, file_entry_create(entry_names[7]));
 
-    file_entry_array_sort(sut);
-    const t_file_entry *first_entry = file_entry_array_get_at(sut, 0);
-    const t_file_entry *second_entry = file_entry_array_get_at(sut, 1);
-    const t_file_entry *third_entry = file_entry_array_get_at(sut, 2);
-    const t_file_entry *fourth_entry = file_entry_array_get_at(sut, 3);
-    const t_file_entry *fifth_entry = file_entry_array_get_at(sut, 4);
-    const t_file_entry *sixth_entry = file_entry_array_get_at(sut, 5);
-    const t_file_entry *seventh_entry = file_entry_array_get_at(sut, 6);
-    const t_file_entry *eighth_entry = file_entry_array_get_at(sut, 7);
+    file_entry_array_sort(array_sut);
+    const t_file_entry *first_entry = file_entry_array_get_at(array_sut, 0);
+    const t_file_entry *second_entry = file_entry_array_get_at(array_sut, 1);
+    const t_file_entry *third_entry = file_entry_array_get_at(array_sut, 2);
+    const t_file_entry *fourth_entry = file_entry_array_get_at(array_sut, 3);
+    const t_file_entry *fifth_entry = file_entry_array_get_at(array_sut, 4);
+    const t_file_entry *sixth_entry = file_entry_array_get_at(array_sut, 5);
+    const t_file_entry *seventh_entry = file_entry_array_get_at(array_sut, 6);
+    const t_file_entry *eighth_entry = file_entry_array_get_at(array_sut, 7);
 
     CU_ASSERT_STRING_EQUAL(file_entry_get_name(first_entry), entry_names[1]);
     CU_ASSERT_STRING_EQUAL(file_entry_get_name(second_entry), entry_names[0]);
@@ -260,7 +244,7 @@ static void should_sort_the_file_operands_by_entry_name(void)
     CU_ASSERT_STRING_EQUAL(file_entry_get_name(sixth_entry), entry_names[5]);
     CU_ASSERT_STRING_EQUAL(file_entry_get_name(seventh_entry), entry_names[7]);
     CU_ASSERT_STRING_EQUAL(file_entry_get_name(eighth_entry), entry_names[6]);
-    CU_ASSERT_PTR_NULL(file_entry_array_get_at(sut, 8));
+    CU_ASSERT_PTR_NULL(file_entry_array_get_at(array_sut, 8));
 }
 
 void register_file_entry_suite(void)
