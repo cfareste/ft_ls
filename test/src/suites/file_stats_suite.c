9624195d8b0b9a0dfdedd74ae4e4d9bcc9589668
cfareste <chrisfidare@yahoo.com>
2026-09-06 12:05:24 +0200

    test: add should_destroy_file_stats_correctly

--- test/src/suites/file_stats_suite.c
+++ test/src/suites/file_stats_suite.c
@@ -56,6 +56,21 @@ static void should_return_NULL_when_creating_file_stats_with_an_empty_path(void)
     CU_ASSERT_PTR_NULL(stats);
 }
 
+static void should_destroy_file_stats_correctly(void)
+{
+    const t_vfs_mock_entry vfs[] = {
+        MOCK_FILE("valid_file"),
+        MOCK_NULL_TERMINATOR()
+    };
+    vfs_mock_setup(vfs);
+
+    t_file_stats *stats = file_stats_create("valid_file");
+
+    file_stats_destroy(&stats);
+
+    CU_ASSERT_PTR_NULL(stats);
+}
+
 static void should_return_an_error_when_retrieving_the_stats_from_a_NULL_path(void)
 {
     const int result = file_stats_get(NULL, &file_stats);
@@ -114,6 +129,7 @@ void register_file_stats_suite(void)
         CU_add_test(suite, "should_create_file_stats_correctly", should_create_file_stats_correctly);
         CU_add_test(suite, "should_return_NULL_when_creating_file_stats_with_an_empty_path", should_return_NULL_when_creating_file_stats_with_an_empty_path);
         CU_add_test(suite, "should_return_NULL_when_creating_file_stats_with_a_NULL_path", should_return_NULL_when_creating_file_stats_with_a_NULL_path);
+        CU_add_test(suite, "should_destroy_file_stats_correctly", should_destroy_file_stats_correctly);
         CU_add_test(suite, "should_return_an_error_when_retrieving_the_stats_from_a_NULL_path", should_return_an_error_when_retrieving_the_stats_from_a_NULL_path);
         CU_add_test(suite, "should_return_an_error_when_retrieving_the_stats_from_an_empty_path", should_return_an_error_when_retrieving_the_stats_from_an_empty_path);
         CU_add_test(suite, "should_return_an_error_when_populating_the_stats_to_a_NULL_stat_struct", should_return_an_error_when_populating_the_stats_to_a_NULL_stat_struct);
