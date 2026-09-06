bceaad491ea9abf2159b592a60248bac1d3969b0
cfareste <chrisfidare@yahoo.com>
2026-09-06 18:52:34 +0200

    refactor: use file_stats_get_file_type to retrieve file operands types

--- inc/file_operands.h
+++ inc/file_operands.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include "file_stats.h"
+
 char **file_operands_get(int num_of_arguments, const char **arguments);
-unsigned int *file_operands_get_types(char **file_operands);
-char **file_operands_get_non_directory(char **file_operands, const unsigned int *file_operands_types);
-char **file_operands_get_directory(char **file_operands, const unsigned int *file_operand_types);
+t_file_type *file_operands_get_types(char **file_operands);
+char **file_operands_get_non_directory(char **file_operands, const t_file_type *file_operands_types);
+char **file_operands_get_directory(char **file_operands, const t_file_type *file_operand_types);
