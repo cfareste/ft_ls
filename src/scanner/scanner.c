fc9ebc7d38fa18f4fdb0eba1cbb438b780dc970f
cfareste <chrisfidare@yahoo.com>
2026-09-07 12:19:37 +0200

    refactor: use new dir_entry logic in scanner

--- src/scanner/scanner.c
+++ src/scanner/scanner.c
@@ -17,16 +17,18 @@ t_file_entry_array *scan(const char *path)
     t_dir_stream *dir_stream = directory_open(path);
 
     t_file_entry_array *file_entry_array = file_entry_array_create();
-    const struct dirent *dir_entry = directory_get_next_entry(dir_stream);
-    while (dir_entry != NULL)
+    t_dir_entry *dir_entry = directory_get_next(dir_stream);
+    while (!directory_is_entry_empty(dir_entry))
     {
-        if (!is_hidden_file(dir_entry->d_name))
+        const char *entry_name = directory_get_entry_name(dir_entry);
+        if (!is_hidden_file(entry_name))
         {
-            t_file_entry *entry = file_entry_create(dir_entry->d_name);
+            t_file_entry *entry = file_entry_create(entry_name);
             file_entry_array_push(file_entry_array, entry);
         }
 
-        dir_entry = directory_get_next_entry(dir_stream);
+        directory_destroy_entry(&dir_entry);
+        dir_entry = directory_get_next(dir_stream);
     }
 
     directory_close(&dir_stream);
