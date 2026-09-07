d70905af5ee1d51becb7f82ef998c79bd22f7fde
cfareste <chrisfidare@yahoo.com>
2026-09-07 11:18:14 +0200

    refactor: reorder tests

--- test/src/suites/directory_suite.c
+++ test/src/suites/directory_suite.c
@@ -55,21 +55,6 @@ static void should_return_a_directory_stream_when_opening_a_valid_path(void)
     CU_ASSERT_PTR_NOT_NULL(dir_stream_sut);
 }
 
-static void should_return_NULL_when_reading_an_entry_from_a_null_directory(void)
-{
-    const t_vfs_mock_entry vfs[] = {
-        MOCK_DIR(VALID_DIRECTORY_PATH, ".", ".."),
-        MOCK_NULL_TERMINATOR()
-    };
-    vfs_mock_setup(vfs);
-
-    open_directory_stream(VALID_DIRECTORY_PATH);
-
-    const struct dirent *entry = directory_get_next_entry(NULL);
-
-    CU_ASSERT_PTR_NULL(entry);
-}
-
 static void should_return_NULL_for_next_entry_if_a_NULL_dir_stream_is_specified(void)
 {
     const t_dir_entry *dir_entry = directory_get_next(NULL);
@@ -93,6 +78,21 @@ static void should_return_the_next_entry_when_reading_from_a_valid_directory(voi
     CU_ASSERT_STRING_EQUAL(directory_get_entry_name(dir_entry), "file");
 
     directory_entry_destroy(&dir_entry);
+}
+
+static void should_return_NULL_when_reading_an_entry_from_a_null_directory(void)
+{
+    const t_vfs_mock_entry vfs[] = {
+        MOCK_DIR(VALID_DIRECTORY_PATH, ".", ".."),
+        MOCK_NULL_TERMINATOR()
+    };
+    vfs_mock_setup(vfs);
+
+    open_directory_stream(VALID_DIRECTORY_PATH);
+
+    const struct dirent *entry = directory_get_next_entry(NULL);
+
+    CU_ASSERT_PTR_NULL(entry);
 }
 
 static void should_return_an_entry_when_reading_from_a_valid_directory(void)
@@ -145,9 +145,9 @@ void register_directory_suite(void)
         CU_add_test(suite, "should_return_NULL_when_opening_a_NULL_path", should_return_NULL_when_opening_a_NULL_path);
         CU_add_test(suite, "should_return_NULL_when_opening_an_empty_path", should_return_NULL_when_opening_an_empty_path);
         CU_add_test(suite, "should_return_a_directory_stream_when_opening_a_valid_path", should_return_a_directory_stream_when_opening_a_valid_path);
-        CU_add_test(suite, "should_return_NULL_when_reading_an_entry_from_a_null_directory", should_return_NULL_when_reading_an_entry_from_a_null_directory);
         CU_add_test(suite, "should_return_NULL_for_next_entry_if_a_NULL_dir_stream_is_specified", should_return_NULL_for_next_entry_if_a_NULL_dir_stream_is_specified);
         CU_add_test(suite, "should_return_the_next_entry_when_reading_from_a_valid_directory", should_return_the_next_entry_when_reading_from_a_valid_directory);
+        CU_add_test(suite, "should_return_NULL_when_reading_an_entry_from_a_null_directory", should_return_NULL_when_reading_an_entry_from_a_null_directory);
         CU_add_test(suite, "should_return_an_entry_when_reading_from_a_valid_directory", should_return_an_entry_when_reading_from_a_valid_directory);
         CU_add_test(suite, "should_return_minus_one_when_closing_a_null_directory", should_return_minus_one_when_closing_a_null_directory);
         CU_add_test(suite, "should_return_zero_when_closing_a_valid_directory", should_return_zero_when_closing_a_valid_directory);
