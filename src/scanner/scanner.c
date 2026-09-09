b2209bab20f883aedf3831023b8bcff8798daf37
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2026-09-09 13:50:14 +0200

    refactor: extract push_entry

--- src/scanner/scanner.c
+++ src/scanner/scanner.c
@@ -4,6 +4,17 @@
 #include "directory.h"
 #include "libft.h"
 
+static void push_entry(t_file_entry_array *file_entry_array, const t_dir_entry *dir_entry)
+{
+    if (directory_is_entry_hidden_file(dir_entry))
+        return ;
+
+    const char *entry_name = directory_get_entry_name(dir_entry);
+    t_file_entry *entry = file_entry_create(entry_name);
+
+    file_entry_array_push(file_entry_array, entry);
+}
+
 t_file_entry_array *scan(const char *path)
 {
     if (!ft_is_valid_path(path))
@@ -15,12 +26,7 @@ t_file_entry_array *scan(const char *path)
     t_dir_entry *dir_entry = directory_get_next_entry(dir_stream);
     while (!directory_is_entry_empty(dir_entry))
     {
-        const char *entry_name = directory_get_entry_name(dir_entry);
-        if (!directory_is_entry_hidden_file(dir_entry))
-        {
-            t_file_entry *entry = file_entry_create(entry_name);
-            file_entry_array_push(file_entry_array, entry);
-        }
+        push_entry(file_entry_array, dir_entry);
 
         directory_destroy_entry(&dir_entry);
         dir_entry = directory_get_next_entry(dir_stream);
