7a8683a59c6ab35cbfbd66315432e6abfec771d9
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2026-09-09 13:38:31 +0200

    refactor: use directory_is_entry_hidden_file in scanner

--- src/scanner/scanner.c
+++ src/scanner/scanner.c
@@ -4,11 +4,6 @@
 #include "directory.h"
 #include "libft.h"
 
-static int is_hidden_file(const char *file_entry_name)
-{
-    return file_entry_name[0] == '.';
-}
-
 t_file_entry_array *scan(const char *path)
 {
     if (!ft_is_valid_path(path))
@@ -21,7 +16,7 @@ t_file_entry_array *scan(const char *path)
     while (!directory_is_entry_empty(dir_entry))
     {
         const char *entry_name = directory_get_entry_name(dir_entry);
-        if (!is_hidden_file(entry_name))
+        if (!directory_is_entry_hidden_file(dir_entry))
         {
             t_file_entry *entry = file_entry_create(entry_name);
             file_entry_array_push(file_entry_array, entry);
