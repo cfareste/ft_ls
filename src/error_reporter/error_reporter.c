369f460d71d281c3f295eb18ec897b499c180a2d
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2026-09-10 16:17:24 +0200

    refactor: extract report_error method

--- src/error_reporter/error_reporter.c
+++ src/error_reporter/error_reporter.c
@@ -4,32 +4,27 @@
 #include "error_reporter.h"
 #include "libft.h"
 
-void report_opening_directory_error(const char *directory_path)
+static void report_error(const char *error_message, const char *file_path)
 {
-    ft_fprintf(STDERR_FILENO, "ft_ls: cannot open directory");
+    ft_fprintf(STDERR_FILENO, "%s", error_message);
 
-    if (!ft_is_str_empty(directory_path))
-        ft_fprintf(STDERR_FILENO, " '%s'", directory_path);
+    if (!ft_is_str_empty(file_path))
+        ft_fprintf(STDERR_FILENO, " '%s'", file_path);
 
     ft_fprintf(STDERR_FILENO, ": %s\n", strerror(errno));
+}
+
+void report_opening_directory_error(const char *directory_path)
+{
+    report_error("ft_ls: cannot open directory", directory_path);
 }
 
 void report_reading_directory_error(const char *directory_path)
 {
-    ft_fprintf(STDERR_FILENO, "ft_ls: reading directory");
-
-    if (!ft_is_str_empty(directory_path))
-        ft_fprintf(STDERR_FILENO, " '%s'", directory_path);
-
-    ft_fprintf(STDERR_FILENO, ": %s\n", strerror(errno));
+    report_error("ft_ls: reading directory", directory_path);
 }
 
 void report_closing_directory_error(const char *directory_path)
 {
-    ft_fprintf(STDERR_FILENO, "ft_ls: closing directory");
-
-    if (!ft_is_str_empty(directory_path))
-        ft_fprintf(STDERR_FILENO, " '%s'", directory_path);
-
-    ft_fprintf(STDERR_FILENO, ": %s\n", strerror(errno));
+    report_error("ft_ls: closing directory", directory_path);
 }
