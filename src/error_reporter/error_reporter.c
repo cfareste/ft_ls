55c8577baf02fbf8f1844911ff6a552e84d3c952
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2026-09-10 15:17:10 +0200

    feat: add NULL message error reporting

--- src/error_reporter/error_reporter.c
+++ src/error_reporter/error_reporter.c
@@ -1,6 +1,11 @@
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
 #include "error_reporter.h"
+#include "libft.h"
 
 void report_error(const char *message)
 {
     (void) message;
+    ft_fprintf(STDERR_FILENO, "ft_ls: %s\n", strerror(errno));
 }
--- test/src/suites/error_reporter_suite.c
+++ test/src/suites/error_reporter_suite.c
@@ -1,7 +1,18 @@
 #include "CUnit/CUnit.h"
 #include "CUnit/Basic.h"
+#include "error_reporter.h"
+#include "mocks.h"
 
 #define SUITE_NAME "error_reporter"
+
+static void should_only_print_the_application_prefix_and_system_error_if_a_NULL_message_is_specified(void)
+{
+    errno = ENOTDIR;
+
+    report_error(NULL);
+
+    CU_ASSERT(verify_that_the_error_printed_is("ft_ls: %s\n", strerror(errno)));
+}
 
 void register_error_reporter_suite(void)
 {
@@ -9,5 +20,6 @@ void register_error_reporter_suite(void)
 
     if (suite != NULL)
     {
+        CU_add_test(suite, "should_only_print_the_application_prefix_and_system_error_if_a_NULL_message_is_specified", should_only_print_the_application_prefix_and_system_error_if_a_NULL_message_is_specified);
     }
 }
