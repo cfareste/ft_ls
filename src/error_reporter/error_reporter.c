fdb3c1a37edc8d90e7d6a0f7b8e4eb77378b6b6e
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2026-09-10 16:18:56 +0200

    refactor: extract error macros

--- src/error_reporter/error_reporter.c
+++ src/error_reporter/error_reporter.c
@@ -4,6 +4,10 @@
 #include "error_reporter.h"
 #include "libft.h"
 
+#define OPENING_DIRECTORY_ERROR "ft_ls: cannot open directory"
+#define READING_DIRECTORY_ERROR "ft_ls: reading directory"
+#define CLOSING_DIRECTORY_ERROR "ft_ls: closing directory"
+
 static void report_error(const char *error_message, const char *file_path)
 {
     ft_fprintf(STDERR_FILENO, "%s", error_message);
@@ -16,15 +20,15 @@ static void report_error(const char *error_message, const char *file_path)
 
 void report_opening_directory_error(const char *directory_path)
 {
-    report_error("ft_ls: cannot open directory", directory_path);
+    report_error(OPENING_DIRECTORY_ERROR, directory_path);
 }
 
 void report_reading_directory_error(const char *directory_path)
 {
-    report_error("ft_ls: reading directory", directory_path);
+    report_error(READING_DIRECTORY_ERROR, directory_path);
 }
 
 void report_closing_directory_error(const char *directory_path)
 {
-    report_error("ft_ls: closing directory", directory_path);
+    report_error(CLOSING_DIRECTORY_ERROR, directory_path);
 }
