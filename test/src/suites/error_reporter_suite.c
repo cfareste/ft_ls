7da9e53d9472c2e651972c14c4b2a8877097a3f2
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2026-09-10 15:18:23 +0200

    test: add should_only_print_the_application_prefix_and_system_error_if_an_empty_message_is_specified

--- test/src/suites/error_reporter_suite.c
+++ test/src/suites/error_reporter_suite.c
@@ -14,6 +14,17 @@ static void should_only_print_the_application_prefix_and_system_error_if_a_NULL_
     CU_ASSERT(verify_that_the_error_printed_is("ft_ls: %s\n", strerror(errno)));
 }
 
+static void should_only_print_the_application_prefix_and_system_error_if_an_empty_message_is_specified(void)
+{
+    reset_printing_buffer();
+
+    errno = ENOENT;
+
+    report_error(NULL);
+
+    CU_ASSERT(verify_that_the_error_printed_is("ft_ls: %s\n", strerror(errno)));
+}
+
 void register_error_reporter_suite(void)
 {
     const CU_pSuite suite = CU_add_suite_with_setup_and_teardown(SUITE_NAME, NULL, NULL, NULL, NULL);
@@ -21,5 +32,6 @@ void register_error_reporter_suite(void)
     if (suite != NULL)
     {
         CU_add_test(suite, "should_only_print_the_application_prefix_and_system_error_if_a_NULL_message_is_specified", should_only_print_the_application_prefix_and_system_error_if_a_NULL_message_is_specified);
+        CU_add_test(suite, "should_only_print_the_application_prefix_and_system_error_if_an_empty_message_is_specified", should_only_print_the_application_prefix_and_system_error_if_an_empty_message_is_specified);
     }
 }
