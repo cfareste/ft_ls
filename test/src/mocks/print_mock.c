52389ed66c63b8908e523f703f84b18778132e4a
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2026-09-10 13:28:37 +0200

    refactor(test): improve error message for printing buffers

--- test/src/mocks/print_mock.c
+++ test/src/mocks/print_mock.c
@@ -9,13 +9,16 @@
 
 static char printing_buffer[BUFFER_COUNT][PRINT_BUFFER_SIZE];
 
-static void print_error(const char *expected, const char *actual)
+static void print_error(const char *expected, const char *actual, const int fd)
 {
-    write(STDERR_FILENO, "\nFAILED:\n\nExpected:\n-----\n", 26);
+    const char *fd_channel = fd == STDOUT_FILENO ? "STDOUT" : "STDERR";
+    write(STDERR_FILENO, "\n|-* ", 5);
+    write(STDERR_FILENO, fd_channel, ft_strlen(fd_channel));
+    write(STDERR_FILENO, " Failed *-|\n\nExpected:\n-----\n", 29);
     write(STDERR_FILENO, expected, ft_strlen(expected));
     write(STDERR_FILENO, "-----\n\nActual:\n-----\n", 21);
     write(STDERR_FILENO, actual, ft_strlen(actual));
-    write(STDERR_FILENO, "-----\n", 6);
+    write(STDERR_FILENO, "-----\n\n|-------------------|\n\n", 30);
 }
 
 int printf_mock(const char *str, ...)
@@ -60,7 +63,7 @@ int verify_that_the_str_that_has_been_printed_is(const char *str, ...)
     const int strings_are_equal = ft_are_string_equals(printing_buffer[STDOUT_FILENO], expected);
 
     if (!strings_are_equal)
-        print_error(expected, printing_buffer[STDOUT_FILENO]);
+        print_error(expected, printing_buffer[STDOUT_FILENO], STDOUT_FILENO);
 
     return strings_are_equal;
 }
@@ -77,7 +80,7 @@ int verify_that_the_error_that_has_been_printed_is(const char *str, ...)
     const int strings_are_equal = ft_are_string_equals(printing_buffer[STDERR_FILENO], expected);
 
     if (!strings_are_equal)
-        print_error(expected, printing_buffer[STDERR_FILENO]);
+        print_error(expected, printing_buffer[STDERR_FILENO], STDERR_FILENO);
 
     return strings_are_equal;
 }
