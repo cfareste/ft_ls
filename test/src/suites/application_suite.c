5e2404b449b5e1b16383cb96e901d8b4655877a5
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2026-09-13 19:08:40 +0200

    test: add should_fail_with_a_major_error_and_print_only_the_working_file_operands_if_some_directories_fail_to_scan

--- test/src/suites/application_suite.c
+++ test/src/suites/application_suite.c
@@ -996,6 +996,55 @@ static void should_fail_with_a_major_error_and_not_print_anything_if_all_directo
     CU_ASSERT_EQUAL(result, FT_LS_APPLICATION_MAJOR_ERROR);
 }
 
+static void should_fail_with_a_major_error_and_print_only_the_working_file_operands_if_some_directories_fail_to_scan(void)
+{
+    const t_vfs_mock_entry vfs[] = {
+        MOCK_DIR_OPEN_ERROR(EACCES, "dir", ".", "..", "file1", "subdir1", "symlink", "zz"),
+        MOCK_DIR_READ_ERROR("zdir/", 4, ".", "..", "file1", "subdir1", "symlink", "zz"),
+        MOCK_FILE("file"),
+        MOCK_FILE("zFile"),
+        MOCK_DIR("workingDir", ".", "..", "fileDir"),
+        MOCK_DIR("zWorkingDir", ".", "..", "zFileDir"),
+        MOCK_NULL_TERMINATOR()
+    };
+    vfs_mock_setup(vfs);
+
+    const char *arguments[] = { "zdir/", "file", "workingDir", "zWorkingDir", "zFile", "dir", NULL };
+    const char *expected_file_names[] = { "file", "zFile" };
+    const char *expected_first_dir_file_name = "fileDir";
+    const char *expected_second_dir_file_name = "zFileDir";
+    const char *expected_failed_dir_file_names[] = { "file1", "subdir1" };
+    parsed_arguments = parse_arguments(6, arguments);
+
+    const int result = application_run(parsed_arguments);
+
+    CU_ASSERT(verify_that_the_output_printed_is(
+        "%s\n%s\n"
+        "\n%s:\n"
+        "%s\n"
+        "\n%s:\n"
+        "%s\n"
+        "\n%s:\n"
+        "%s\n%s\n",
+        expected_file_names[0],
+        expected_file_names[1],
+        arguments[2],
+        expected_first_dir_file_name,
+        arguments[3],
+        expected_second_dir_file_name,
+        arguments[0],
+        expected_failed_dir_file_names[0],
+        expected_failed_dir_file_names[1]
+    ));
+    CU_ASSERT(verify_that_the_error_printed_is(
+        "ft_ls: cannot open directory '%s': %s\n"
+        "ft_ls: reading directory '%s': %s\n",
+        "dir", strerror(EACCES),
+        "zdir/", strerror(EBADF))
+    );
+    CU_ASSERT_EQUAL(result, FT_LS_APPLICATION_MAJOR_ERROR);
+}
+
 void register_application_suite(void)
 {
     const CU_pSuite suite = CU_add_suite_with_setup_and_teardown(SUITE_NAME, NULL, NULL, test_setup, test_teardown);
@@ -1034,5 +1083,6 @@ void register_application_suite(void)
         CU_add_test(suite, "should_fail_with_a_major_error_and_print_only_the_working_directory_operand_if_fails_to_scan_a_directory_operand", should_fail_with_a_major_error_and_print_only_the_working_directory_operand_if_fails_to_scan_a_directory_operand);
         CU_add_test(suite, "should_fail_with_a_major_error_and_print_only_the_working_file_operands_if_fails_to_scan_a_directory_operand", should_fail_with_a_major_error_and_print_only_the_working_file_operands_if_fails_to_scan_a_directory_operand);
         CU_add_test(suite, "should_fail_with_a_major_error_and_not_print_anything_if_all_directories_fail_to_scan", should_fail_with_a_major_error_and_not_print_anything_if_all_directories_fail_to_scan);
+        CU_add_test(suite, "should_fail_with_a_major_error_and_print_only_the_working_file_operands_if_some_directories_fail_to_scan", should_fail_with_a_major_error_and_print_only_the_working_file_operands_if_some_directories_fail_to_scan);
     }
 }
