c06ce08b055517b0981c89baf22508c1f211b933
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2026-09-14 11:38:43 +0200

    refactor: use render_directory in application

--- src/application/application.c
+++ src/application/application.c
@@ -36,11 +36,7 @@ static int process_directory_file_operands(const t_parsed_arguments *parsed_argu
         file_entry_array_sort(file_entry_array);
 
         if (file_entry_array != NULL)
-        {
-            render_context_set_directory_header(render_context, directory_file_operands[i]);
-            render_directory_header(render_context);
-            render(file_entry_array, render_context);
-        }
+            render_directory(render_context, directory_file_operands[i], file_entry_array);
 
         result_destroy(&result);
         file_entry_array_destroy(&file_entry_array);
