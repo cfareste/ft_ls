3d8471eb2c596a2bb8f9bb389ae7a35525a5d9d7
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2026-09-18 19:33:34 +0200

    test: add should_fail_with_a_major_error_and_not_print_anything_if_one_file_operand_fails_to_be_accessed

--- test/src/suites/application_suite.c
+++ test/src/suites/application_suite.c
@@ -1073,6 +1073,31 @@ static void should_fail_with_a_major_error_and_not_print_anything_if_the_specifi
     CU_ASSERT_EQUAL(result, FT_LS_APPLICATION_MAJOR_ERROR);
 }
 
+static void should_fail_with_a_major_error_and_not_print_anything_if_one_file_operand_fails_to_be_accessed(void)
+{
+    const t_vfs_mock_entry vfs[] = {
+        MOCK_FILE_ACCESS_ERROR(EACCES, "no/perms"),
+        MOCK_DIR("dir", ".", "..", "file"),
+        MOCK_NULL_TERMINATOR()
+    };
+    vfs_mock_setup(vfs);
+
+    const char *arguments[] = { "no/perms", "dir", NULL };
+    get_parsed_arguments_result(2, arguments);
+
+    const int result = application_run(parsed_arguments_result);
+
+    CU_ASSERT(verify_that_the_output_printed_is(
+        "%s:\n%s\n",
+        "dir", "file"
+    ));
+    CU_ASSERT(verify_that_the_error_printed_is(
+        "ft_ls: cannot access '%s': %s\n",
+        "no/perms", strerror(EACCES))
+    );
+    CU_ASSERT_EQUAL(result, FT_LS_APPLICATION_MAJOR_ERROR);
+}
+
 void register_application_suite(void)
 {
     const CU_pSuite suite = CU_add_suite_with_setup_and_teardown(SUITE_NAME, NULL, NULL, test_setup, test_teardown);
@@ -1113,5 +1138,6 @@ void register_application_suite(void)
         CU_add_test(suite, "should_fail_with_a_major_error_and_not_print_anything_if_all_directories_fail_to_scan", should_fail_with_a_major_error_and_not_print_anything_if_all_directories_fail_to_scan);
         CU_add_test(suite, "should_fail_with_a_major_error_and_print_only_the_working_file_operands_if_some_directories_fail_to_scan", should_fail_with_a_major_error_and_print_only_the_working_file_operands_if_some_directories_fail_to_scan);
         CU_add_test(suite, "should_fail_with_a_major_error_and_not_print_anything_if_the_specified_file_operand_fails_to_be_accessed", should_fail_with_a_major_error_and_not_print_anything_if_the_specified_file_operand_fails_to_be_accessed);
+        CU_add_test(suite, "should_fail_with_a_major_error_and_not_print_anything_if_one_file_operand_fails_to_be_accessed", should_fail_with_a_major_error_and_not_print_anything_if_one_file_operand_fails_to_be_accessed);
     }
 }
