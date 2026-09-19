289cc79c64f043c1f3909b2557f14f56a206ccd6
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2026-09-19 13:35:14 +0200

    refactor: rename t_ft_ls_error_code

--- inc/application.h
+++ inc/application.h
@@ -7,6 +7,6 @@ typedef enum e_ft_ls_application_status
     FT_LS_APPLICATION_SUCCESS,
     FT_LS_APPLICATION_MINOR_ERROR,
     FT_LS_APPLICATION_MAJOR_ERROR
-} t_ft_ls_application_status;
+} t_ft_ls_error_code;
 
-t_ft_ls_application_status application_run(const t_result *parsing_arguments_result);
+t_ft_ls_error_code application_run(const t_result *parsing_arguments_result);
