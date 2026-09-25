e4f4540a323588861f3ad72fb5700b57403874ec
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2026-09-25 13:59:19 +0200

    test: add should_return_false_for_has_option_if_the_specified_option_was_NOT_parsed

--- test/src/suites/parsed_arguments_suite.c
+++ test/src/suites/parsed_arguments_suite.c
@@ -238,6 +238,24 @@ static void should_return_true_for_has_any_option_if_at_least_one_option_was_spe
     CU_ASSERT(verify_that_no_error_was_printed());
 }
 
+static void should_return_false_for_has_option_if_the_specified_option_was_NOT_parsed(void)
+{
+    const t_vfs_mock_entry vfs[] = {
+        MOCK_DIR("dir", ".", ".."),
+        MOCK_NULL_TERMINATOR()
+    };
+    vfs_mock_setup(vfs);
+
+    const char *args[] = { "dir", "-l", NULL };
+    get_parsed_arguments_result(2, args);
+
+    const int has_option = parsed_arguments_has_option(sut, OPTIONS_RECURSIVE);
+
+    CU_ASSERT_FALSE(has_option);
+    CU_ASSERT_TRUE(result_has_succeed(parsed_arguments_result));
+    CU_ASSERT(verify_that_no_error_was_printed());
+}
+
 static void should_return_true_for_has_option_if_the_specified_option_was_parsed(void)
 {
     const t_vfs_mock_entry vfs[] = {
@@ -249,9 +267,9 @@ static void should_return_true_for_has_option_if_the_specified_option_was_parsed
     const char *args[] = { "file", "-R", NULL };
     get_parsed_arguments_result(2, args);
 
-    const int has_any_option = parsed_arguments_has_option(sut, OPTIONS_RECURSIVE);
+    const int has_option = parsed_arguments_has_option(sut, OPTIONS_RECURSIVE);
 
-    CU_ASSERT_TRUE(has_any_option);
+    CU_ASSERT_TRUE(has_option);
     CU_ASSERT_TRUE(result_has_succeed(parsed_arguments_result));
     CU_ASSERT(verify_that_no_error_was_printed());
 }
@@ -709,6 +727,7 @@ void register_parsed_arguments_suite(void)
         CU_add_test(suite, "should_return_false_for_has_any_option_if_NULL_parsed_arguments_are_passed", should_return_false_for_has_any_option_if_NULL_parsed_arguments_are_passed);
         CU_add_test(suite, "should_return_false_for_has_any_option_if_at_no_option_was_specified", should_return_false_for_has_any_option_if_at_no_option_was_specified);
         CU_add_test(suite, "should_return_true_for_has_any_option_if_at_least_one_option_was_specified", should_return_true_for_has_any_option_if_at_least_one_option_was_specified);
+        CU_add_test(suite, "should_return_false_for_has_option_if_the_specified_option_was_NOT_parsed", should_return_false_for_has_option_if_the_specified_option_was_NOT_parsed);
         CU_add_test(suite, "should_return_true_for_has_option_if_the_specified_option_was_parsed", should_return_true_for_has_option_if_the_specified_option_was_parsed);
         CU_add_test(suite, "should_return_false_for_multiple_file_operands_if_NULL_parsed_arguments_are_passed", should_return_false_for_multiple_file_operands_if_NULL_parsed_arguments_are_passed);
         CU_add_test(suite, "should_return_false_for_multiple_file_operands_if_has_less_than_two", should_return_false_for_multiple_file_operands_if_has_less_than_two);
