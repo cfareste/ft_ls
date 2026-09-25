3a22c5e3bcada118e0cfd74254b5ed1d352b46fc
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2026-09-25 14:33:13 +0200

    test: add should_parse_the_option_and_file_operands_if_the_option_was_the_first_specified_argument

--- test/src/suites/parsed_arguments_suite.c
+++ test/src/suites/parsed_arguments_suite.c
@@ -494,6 +494,32 @@ static void should_sort_the_directory_file_operands_by_ascii_by_default(void)
     CU_ASSERT(verify_that_no_error_was_printed());
 }
 
+static void should_parse_the_option_and_file_operands_if_the_option_was_the_first_specified_argument(void)
+{
+    const t_vfs_mock_entry vfs[] = {
+        MOCK_FILE("file"),
+        MOCK_FILE("file2"),
+        MOCK_DIR("dir", ".", ".."),
+        MOCK_NULL_TERMINATOR()
+    };
+    vfs_mock_setup(vfs);
+
+    const char *arguments[] = { "-R", "file", "file2", "dir", NULL };
+
+    get_parsed_arguments_result(4, arguments);
+    const char * const *non_directory_file_operands = parsed_arguments_get_non_directory_file_operands(sut);
+    const char * const *directory_file_operands = parsed_arguments_get_directory_file_operands(sut);
+
+    CU_ASSERT_TRUE(result_has_succeed(parsed_arguments_result));
+    CU_ASSERT_TRUE(parsed_arguments_has_option(sut, OPTIONS_RECURSIVE));
+    CU_ASSERT_STRING_EQUAL(non_directory_file_operands[0], "file");
+    CU_ASSERT_STRING_EQUAL(non_directory_file_operands[1], "file2");
+    CU_ASSERT_PTR_NULL(non_directory_file_operands[2]);
+    CU_ASSERT_STRING_EQUAL(directory_file_operands[0], "dir");
+    CU_ASSERT_PTR_NULL(directory_file_operands[1]);
+    CU_ASSERT(verify_that_no_error_was_printed());
+}
+
 static void should_parse_the_recursive_option(void)
 {
     const char *arguments[] = { "-R", NULL };
@@ -746,6 +772,7 @@ void register_parsed_arguments_suite(void)
         CU_add_test(suite, "should_return_true_for_has_mixed_types_file_operands_if_it_does_have_at_least_one_non_dir_and_one_dir", should_return_true_for_has_mixed_types_file_operands_if_it_does_have_at_least_one_non_dir_and_one_dir);
         CU_add_test(suite, "should_sort_the_non_directory_file_operands_by_ascii_by_default", should_sort_the_non_directory_file_operands_by_ascii_by_default);
         CU_add_test(suite, "should_sort_the_directory_file_operands_by_ascii_by_default", should_sort_the_directory_file_operands_by_ascii_by_default);
+        CU_add_test(suite, "should_parse_the_option_and_file_operands_if_the_option_was_the_first_specified_argument", should_parse_the_option_and_file_operands_if_the_option_was_the_first_specified_argument);
         CU_add_test(suite, "should_parse_the_recursive_option", should_parse_the_recursive_option);
         //------
         CU_add_test(suite, "should_be_created_correctly_even_if_current_directory_is_invalid_if_no_parameters_were_specified", should_be_created_correctly_even_if_current_directory_is_invalid_if_no_parameters_were_specified);
