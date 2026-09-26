287591f2d937dabbe147f2d458218100ef1ec7bf
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2026-09-26 19:01:57 +0200

    test: add should_return_NULL_if_directory_path_is_NULL

--- test/src/suites/path_builder_suite.c
+++ test/src/suites/path_builder_suite.c
@@ -4,11 +4,19 @@
 
 #define SUITE_NAME "path_builder"
 
+static void should_return_NULL_if_directory_path_is_NULL(void)
+{
+    const char *path = build_path(NULL, "valid_path");
+
+    CU_ASSERT_PTR_NULL(path);
+}
+
 void register_path_builder_suite(void)
 {
     const CU_pSuite suite = CU_add_suite_with_setup_and_teardown(SUITE_NAME, NULL, NULL, NULL, NULL);
 
     if (suite != NULL)
     {
+        CU_add_test(suite, "should_return_NULL_if_directory_path_is_NULL", should_return_NULL_if_directory_path_is_NULL);
     }
 }
